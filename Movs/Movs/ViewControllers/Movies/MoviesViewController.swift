66018a7d1a8e6756c9c97dab37496dbeffa9bea6
Dielson Sales <dielsonscarvalho@gmail.com>
2018-09-29 18:00:33 -0300

    Create MoviesViewController and FavoritesViewController

---
+++ Movs/Movs/ViewControllers/Movies/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesViewController.swift
+//  Movs
+//
+//  Created by Dielson Sales on 29/09/18.
+//  Copyright © 2018 Dielson Sales. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
