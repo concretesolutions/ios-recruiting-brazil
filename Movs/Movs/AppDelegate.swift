39b2988eecf0c5dbffc2570f4cf57b0ef2112145
Dielson Sales <dielsonscarvalho@gmail.com>
2018-09-29 18:18:47 -0300

    Remove Storyboard file
    Instead, the UITabBarController is gonna be instantiated in the
    AppDelegate.swift.

--- Movs/Movs/AppDelegate.swift
+++ Movs/Movs/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [
+            MoviesViewController(),
+            FavoritesViewController()
+        ]
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
         return true
     }
 
