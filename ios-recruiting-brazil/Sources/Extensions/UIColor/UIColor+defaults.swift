41dcf8ff2f0289768f6578f7eaefde9303bb3d61
Andre Vieira <vieira.agp@gmail.com>
2018-09-30 09:46:52 -0300

    create color pallete

---
+++ ios-recruiting-brazil/Sources/Extensions/UIColor/UIColor+defaults.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+defaults.swift
+//  ios-recruiting-brazil
+//
+//  Created by André Vieira on 30/09/18.
+//  Copyright © 2018 André Vieira. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var lightYellow: UIColor {
+        return UIColor(red: 247.0/255.0, green: 206.0/255.0, blue: 91.0/255.0, alpha: 1)
+    }
+    
+    static var darkBlue: UIColor {
+        return UIColor(red: 45.0/255.0, green: 48.0/255.0, blue: 71.0/255.0, alpha: 1)
+    }
+    
+    static var darkYellow: UIColor {
+        return UIColor(red: 217.0/255.0, green: 151.0/255.0, blue: 30.0/255.0, alpha: 1)
+    }
+}
