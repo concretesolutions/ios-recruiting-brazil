8823a1f525cad581939c31e5ae2f91684ab980d5
Dielson Sales <dielsonscarvalho@gmail.com>
2018-09-30 21:33:21 -0300

    Add correct title and refactor constants in MovieDetails

--- Movs/Movs/ViewControllers/MovieDetails/MovieDetailsViewController.swift
+++ Movs/Movs/ViewControllers/MovieDetails/MovieDetailsViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class MovieDetailsViewController: UIViewController {
 
+    enum Constants {
+        static let nibName = "MovieDetailsViewController"
+        static let title = "Movie Details"
+    }
+
     @IBOutlet weak var imageHeight: NSLayoutConstraint!
     @IBOutlet weak var favoriteButton: UIButton!
 
@@ -17,6 +22,15 @@ class MovieDetailsViewController: UIViewController {
     @IBOutlet weak var lbYear: UILabel!
     @IBOutlet weak var lbCategory: UILabel!
     @IBOutlet weak var lbDescription: UILabel!
+
+    init() {
+        super.init(nibName: Constants.nibName, bundle: nil)
+        self.title = Constants.title
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +46,6 @@ class MovieDetailsViewController: UIViewController {
             + " aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"
             + " dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia"
             + " deserunt mollit anim id est laborum."
-        lbDescription.sizeToFit()
     }
 
     private func setupViews() {
