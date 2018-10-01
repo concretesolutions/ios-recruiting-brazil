64f7a8c10c8692cfb31af253c8b991d4dbbbe74b
Dielson Sales <dielsonscarvalho@gmail.com>
2018-10-01 01:29:38 -0300

    Create Favorites list and cell

--- Movs/Movs/ViewControllers/Favorites/FavoritesViewController.swift
+++ Movs/Movs/ViewControllers/Favorites/FavoritesViewController.swift
@@ -13,7 +13,10 @@ class FavoritesViewController: UIViewController {
     enum Constants {
         static let nibName = "FavoritesViewController"
         static let title = "Favorites"
+        static let cellNibName = "FavoritesTableViewCell"
     }
+
+    @IBOutlet var favoritesTableView: UITableView!
 
     init() {
         super.init(nibName: Constants.nibName, bundle: nil)
@@ -28,5 +31,44 @@ class FavoritesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        favoritesTableView.register(
+            UINib(nibName: Constants.cellNibName, bundle: nil),
+            forCellReuseIdentifier: Constants.cellNibName
+        )
+        favoritesTableView.estimatedRowHeight = 100
+        favoritesTableView.dataSource = self
+        favoritesTableView.delegate = self
+    }
+}
+
+extension FavoritesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: Constants.cellNibName,
+            for: indexPath
+        )
+        guard let favoriteCell = cell as? FavoritesTableViewCell else {
+            fatalError("Cell not configured")
+        }
+        // TODO: setup cell
+        return favoriteCell
+    }
+}
+
+extension FavoritesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
