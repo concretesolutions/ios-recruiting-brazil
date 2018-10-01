a4c30b7cd35f5a72d8d76da8fb19e30659659952
Dielson Sales <dielsonscarvalho@gmail.com>
2018-09-30 22:20:34 -0300

    Adjust navigation bar and tab bar colors

--- Movs/Movs/AppDelegate.swift
+++ Movs/Movs/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        setupAppearances()
+
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = [
             UINavigationController(rootViewController: MoviesViewController()),
@@ -55,5 +56,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also
         // applicationDidEnterBackground:.
+    }
+
+    // MARK: - Private functions
+
+    private func setupAppearances() {
+        UINavigationBar.appearance().barTintColor = GlobalConstants.Colors.goldenOrange
+        UINavigationBar.appearance().tintColor = UIColor.black
+
+        UITabBar.appearance().barTintColor = GlobalConstants.Colors.goldenOrange
+        UITabBar.appearance().tintColor = UIColor.black
     }
 }
