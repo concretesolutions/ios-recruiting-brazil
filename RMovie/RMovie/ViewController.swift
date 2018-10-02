bbf23ac387bc5877ae6c34c332fe2bf65a76d633
Renato Augusto Mendes Nogueira Mori <renatomori@mbp-de-renato.lan>
2018-10-01 21:44:27 -0300

    criado projeto RMovie

---
+++ RMovie/RMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RMovie
+//
+//  Created by Renato Mori on 01/10/2018.
+//  Copyright © 2018 Renato Mori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
