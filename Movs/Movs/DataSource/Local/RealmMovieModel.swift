cfcea2928100e7fd7fa98957645237c8a1f66b15
Dielson Sales <dielsonscarvalho@gmail.com>
2018-10-04 20:12:33 -0300

    Add Realm library to the project

---
+++ Movs/Movs/DataSource/Local/RealmMovieModel.swift
@@ -0,0 +1,13 @@
+//
+//  RealmMovieModel.swift
+//  Movs
+//
+//  Created by Dielson Sales on 04/10/18.
+//  Copyright © 2018 Dielson Sales. All rights reserved.
+//
+
+import UIKit
+
+class RealmMovieModel: NSObject {
+
+}
