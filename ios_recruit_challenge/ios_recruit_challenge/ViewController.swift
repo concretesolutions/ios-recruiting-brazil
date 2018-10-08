cc589d1a098e01be13d8d002c60b84092d961f55
Lucas de Brito <Lucas@Lucas-de-Brito-2.local>
2018-10-08 19:44:45 -0300

    Cria projeto básico

---
+++ ios_recruit_challenge/ios_recruit_challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_recruit_challenge
+//
+//  Created by Lucas de Brito on 08/10/2018.
+//  Copyright © 2018 Lucas de Brito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
