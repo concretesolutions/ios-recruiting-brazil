0fab2853b24c531308365e1b4831b8a8d9c3c014
Gabriel Reynoso <folameloma@gmail.com>
2018-10-21 00:57:35 -0300

    Created scenes directories and files

---
+++ Movs/Movs/Scenes/MovieDetail/MovieDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MovieDetailViewController.swift
+//  Movs
+//
+//  Created by Gabriel Reynoso on 21/10/18.
+//  Copyright © 2018 Gabriel Reynoso. All rights reserved.
+//
+
+import Foundation
