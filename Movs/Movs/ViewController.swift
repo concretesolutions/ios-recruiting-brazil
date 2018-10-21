a0d04258cd73513a28223b10a8e96a5fb21f08a4
Gabriel Reynoso <folameloma@gmail.com>
2018-10-20 22:23:27 -0300

    Created project, created .gitignore

---
+++ Movs/Movs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movs
+//
+//  Created by Gabriel Reynoso on 20/10/18.
+//  Copyright © 2018 Gabriel Reynoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
