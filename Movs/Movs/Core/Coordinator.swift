5d7e8f529cdfabc5d04929dec77e790074bb99d7
Gabriel Reynoso <folameloma@gmail.com>
2018-10-21 00:47:43 -0300

    Created Coordinator protocol

---
+++ Movs/Movs/Core/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  Movs
+//
+//  Created by Gabriel Reynoso on 21/10/18.
+//  Copyright © 2018 Gabriel Reynoso. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    func start()
+}
