61bd1b5f9d07d8cf4609026561e3a3da2262d33b
Ricardo Rachaus <ricardorachaus@gmail.com>
2018-10-23 15:25:30 -0300

    Changing background color

--- Movs/Movs/ViewController.swift
+++ Movs/Movs/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
     }
 
 
