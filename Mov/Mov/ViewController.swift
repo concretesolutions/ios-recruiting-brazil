f91d87f22b6a678e9125005d690839423c86c6f6
Miguel Nery <mik@Miguels-MacBook-Pro.local>
2018-10-25 15:39:21 -0300

    add app project

---
+++ Mov/Mov/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mov
+//
+//  Created by Miguel Nery on 25/10/18.
+//  Copyright © 2018 Miguel Nery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
