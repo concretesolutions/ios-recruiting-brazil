83275a77d231ad9b794b0d5c84b67139581b3ed9
Gabriel Reynoso <folameloma@gmail.com>
2018-10-25 16:15:57 -0300

    Changed navigation bar to setup at init instead of view did load

--- Movs/Movs/UI/NavigationBar/MovsNavigationController.swift
+++ Movs/Movs/UI/NavigationBar/MovsNavigationController.swift
@@ -12,8 +12,27 @@ final class MovsNavigationController: UINavigationController {
     
     private var searchController:UISearchController!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+        self.commonInit()
+    }
+    
+    override init(navigationBarClass: AnyClass?, toolbarClass: AnyClass?) {
+        super.init(navigationBarClass: navigationBarClass, toolbarClass: toolbarClass)
+        self.commonInit()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+    
+    private func commonInit() {
         self.navigationBar.prefersLargeTitles = true
         self.navigationBar.backgroundColor = Colors.mainYellow.color
         self.navigationBar.barTintColor = Colors.mainYellow.color
