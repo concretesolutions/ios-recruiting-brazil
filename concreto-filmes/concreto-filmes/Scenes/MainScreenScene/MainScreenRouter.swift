6e03725d1378c51e6a7d9a2ae02a8ba2668ffb51
Leonel Menezes <mml_leo@hotmail.com>
2018-10-27 00:00:10 -0300

    adjusted main screen router brackets

--- concreto-filmes/concreto-filmes/Scenes/MainScreenScene/MainScreenRouter.swift
+++ concreto-filmes/concreto-filmes/Scenes/MainScreenScene/MainScreenRouter.swift
@@ -12,49 +12,48 @@
 
 import UIKit
 
-@objc protocol MainScreenRoutingLogic
-{
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
+@objc protocol MainScreenRoutingLogic{
+    //func routeToSomewhere(segue: UIStoryboardSegue?)
 }
 
-protocol MainScreenDataPassing
-{
-  var dataStore: MainScreenDataStore? { get }
+protocol MainScreenDataPassing{
+    var dataStore: MainScreenDataStore? { get }
 }
 
-class MainScreenRouter: NSObject, MainScreenRoutingLogic, MainScreenDataPassing
-{
-  weak var viewController: MainScreenViewController?
-  var dataStore: MainScreenDataStore?
-  
-  // MARK: Routing
-  
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
-  //{
-  //  if let segue = segue {
-  //    let destinationVC = segue.destination as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //  } else {
-  //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-  //    let destinationVC = storyboard.instantiateViewController(withIdentifier: "SomewhereViewController") as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //    navigateToSomewhere(source: viewController!, destination: destinationVC)
-  //  }
-  //}
-
-  // MARK: Navigation
-  
-  //func navigateToSomewhere(source: MainScreenViewController, destination: SomewhereViewController)
-  //{
-  //  source.show(destination, sender: nil)
-  //}
-  
-  // MARK: Passing data
-  
-  //func passDataToSomewhere(source: MainScreenDataStore, destination: inout SomewhereDataStore)
-  //{
-  //  destination.name = source.name
-  //}
+class MainScreenRouter: NSObject, MainScreenRoutingLogic, MainScreenDataPassing{
+    weak var viewController: MainScreenViewController?
+    var dataStore: MainScreenDataStore?
+    
+    // MARK: Routing
+    
+    
+    //func routeToSomewhere(segue: UIStoryboardSegue?)
+    //{
+    //  if let segue = segue {
+    //    let destinationVC = segue.destination as! SomewhereViewController
+    //    var destinationDS = destinationVC.router!.dataStore!
+    //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
+    //  } else {
+    //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    //    let destinationVC = storyboard.instantiateViewController(withIdentifier: "SomewhereViewController") as! SomewhereViewController
+    //    var destinationDS = destinationVC.router!.dataStore!
+    //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
+    //    navigateToSomewhere(source: viewController!, destination: destinationVC)
+    //  }
+    //}
+    
+    // MARK: Navigation
+    
+    //func navigateToSomewhere(source: MainScreenViewController, destination: SomewhereViewController)
+    //{
+    //  source.show(destination, sender: nil)
+    //}
+    
+    // MARK: Passing data
+    
+    //func passDataToSomewhere(source: MainScreenDataStore, destination: inout SomewhereDataStore)
+    //{
+    //  destination.name = source.name
+    //}
 }
+
