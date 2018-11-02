b7f654f02ba02c382ae7aa3d2b77f68ddbdea308
math222ribeiro <math222ribeiro@hotmail.com>
2018-11-02 03:11:57 -0300

    Loading indicators added to favorite movie cell

--- Movies/Movies/Views/FavoriteTableViewCell.swift
+++ Movies/Movies/Views/FavoriteTableViewCell.swift
@@ -11,6 +11,8 @@ import Reusable
 
 class FavoriteTableViewCell: UITableViewCell, NibReusable {
   
+  @IBOutlet weak var genresActivityIndicatorView: UIActivityIndicatorView!
+  @IBOutlet weak var posterDownloadActivityIndicatorView: UIActivityIndicatorView!
   @IBOutlet weak var posterImageView: UIImageView!
   @IBOutlet weak var genresLabel: UILabel!
   @IBOutlet weak var titleLabel: UILabel!
@@ -27,17 +29,56 @@ class FavoriteTableViewCell: UITableViewCell, NibReusable {
     posterImageView.layer.borderWidth = 1
   }
   
+  enum PosterDownloadState {
+    case loading
+    case loaded
+  }
+  
+  enum GenreLoadingState {
+    case loading
+    case loaded
+  }
+  
+  var currentPosterDownloadState: PosterDownloadState! {
+    didSet {
+      switch currentPosterDownloadState! {
+      case .loaded:
+        posterDownloadActivityIndicatorView.stopAnimating()
+        posterDownloadActivityIndicatorView.isHidden = true
+      case .loading:
+        posterDownloadActivityIndicatorView.startAnimating()
+        posterDownloadActivityIndicatorView.isHidden = false
+      }
+    }
+  }
+  
+  var currentGenreLoadingState: GenreLoadingState! {
+    didSet {
+      switch currentGenreLoadingState! {
+      case .loaded:
+        genresActivityIndicatorView.stopAnimating()
+        genresActivityIndicatorView.isHidden = true
+      case .loading:
+        genresActivityIndicatorView.startAnimating()
+        genresActivityIndicatorView.isHidden = false
+      }
+    }
+  }
+  
   func configure(withMovie movie: Movie) {
     self.movie = movie
+    
+    currentPosterDownloadState = .loading
     posterImageView.sd_setImage(with: NetworkClient.shared.getImageDownloadURL(fromPath: movie.posterPath)) { (_, _, _, _) in
-      //      self.currentLoadingState = .loaded
+      self.currentPosterDownloadState = .loaded
     }
     
+    currentGenreLoadingState = .loading
     NetworkClient.shared.getGenres { (result) in
       switch result {
       case .success(let genres):
         self.genresLabel.text = self.getGenresName(fromGenresArray: genres).joined(separator: ", ")
-      //        self.currentGenreLoadingState = .loaded
+        self.currentGenreLoadingState = .loaded
       case .failure:
         print("error")
       }
