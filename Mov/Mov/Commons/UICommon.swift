55c797458ee6b28cc080b788c5e6d2c4e0d99180
Miguel Nery <nery.mik@gmail.com>
2018-11-03 17:05:14 -0300

    stable version 1.0

--- Mov/Mov/Commons/UICommon.swift
+++ Mov/Mov/Commons/UICommon.swift
@@ -13,7 +13,7 @@ struct UICommon {
     private init() {}
     
     static var favoritesErrorAlert: UIAlertController {
-        let alert = UIAlertController(title: "Erro", message: "A memória de Favoritos falhou. Nos perdoe.", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Erro", message: "Um erro ocorreu. Por favor, tente esta ação mais tarde", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Fechar", style: .default, handler: nil))
         
         return alert
