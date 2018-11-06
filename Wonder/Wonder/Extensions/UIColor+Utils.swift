b5e37f0d0b06afab176b23a5453defc0e40980d1
Marcelo Sampaio <marcelo@artlogica.com.br>
2018-11-06 17:06:19 -0200

    working on project setup

---
+++ Wonder/Wonder/Extensions/UIColor+Utils.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Utils.swift
+//  Wonder
+//
+//  Created by Marcelo on 06/11/18.
+//  Copyright © 2018 Marcelo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension UIColor {
+    
+    class var applicationBarTintColor: UIColor {
+        get {
+            return UIColor(red: 247/255, green: 206/255, blue: 92/255, alpha: 1)
+        }
+    }
+    
+    
+    
+}
--- Wonder/Wonder/Root/AppDelegate.swift
+++ Wonder/Wonder/Root/AppDelegate.swift
@@ -16,7 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Global Tint Color
+        window?.tintColor = UIColor.darkGray
+        
+        // Global Navigation Bar Tint Color
+        UINavigationBar.appearance().tintColor = UIColor.black
+        UINavigationBar.appearance().barTintColor = UIColor.applicationBarTintColor
+        
+        
         return true
     }
 
