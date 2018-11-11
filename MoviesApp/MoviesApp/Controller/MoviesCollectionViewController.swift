f2cc9e4bc2d71584f85411b0142f210cf2d0327a
André Antonio Faruolo Filho <andre_antonio1900@hotmail.com>
2018-11-11 19:30:29 -0200

    Added: Initial Storyboard layout; AppIcon & Icons. Did preliminar work on file arrangements.

---
+++ MoviesApp/MoviesApp/Controller/MoviesCollectionViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MoviesCollectionViewController.swift
+//  MoviesApp
+//
+//  Created by Andre Faruolo on 11/11/18.
+//  Copyright © 2018 Andre Faruolo. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class MoviesCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+
+}
