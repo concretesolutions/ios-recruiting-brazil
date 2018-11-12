f12c97ebf4db838ee1cad4aebc6fdd5c16587d81
adannsergio <adannsergio@gmail.com>
2018-11-12 10:42:59 -0200

    Added new project, .yml for lint and .gitignore

---
+++ Movs/Movs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movs
+//
+//  Created by Adann Simões on 12/11/18.
+//  Copyright © 2018 SAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
