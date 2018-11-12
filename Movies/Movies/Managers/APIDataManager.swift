7a8f0e43d99e9879e9a690bbd4a491975897e154
rsg73626 <renan.rsg@hotmail.com>
2018-11-12 11:37:49 -0200

    Implementing APIDataManager.readPopular function.

--- Movies/Movies/Managers/APIDataManager.swift
+++ Movies/Movies/Managers/APIDataManager.swift
@@ -15,14 +15,31 @@ class APIDataManager {
         private static let apiKey: String = "a843669bd8c7b0e6643bbd5be9dcacb3"
         private static let host: String = "https://api.themoviedb.org/3"
         
-        static var readPopular: String { return host + "/movie/popular?api_key=" + apiKey}
+        static var readPopular: String { return host + "/movie/popular?api_key=" + apiKey }
+        static var readGenres: String { return host + "/genre/movie/list?api_key=" + apiKey }
         
     }
     
     // MARK: - Functions
     
     static func readPopular(callback: @escaping ([Movie])->()) {
+        let url = URL(string: RequestURL.readPopular)!
         
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let data = data {
+                let decoder = JSONDecoder()
+                do {
+                    let movies = try decoder.decode([Movie].self, from: data)
+                    callback(movies)
+                } catch {
+                    print("Impossible to decode to Movie from data.")
+                }
+            }
+        }
     }
     
     static func readGenres(callback: @escaping ([Genre])->()) {
