d1378c91b91adcd3f7e2aef13ecfeaecfbbf7baa
rsg73626 <renan.rsg@hotmail.com>
2018-11-12 10:08:17 -0200

    First Commit - Project basic organization.

---
+++ Movies/Movies/Managers/APIDataManager.swift
@@ -0,0 +1,22 @@
+//
+//  APIDataManager.swift
+//  Movies
+//
+//  Created by Renan Germano on 12/11/2018.
+//  Copyright © 2018 Renan Germano. All rights reserved.
+//
+
+import Foundation
+
+class APIDataManager {
+    
+    // MARK: - Types definition
+    struct RequestURL {
+        private static let apiKey: String = "a843669bd8c7b0e6643bbd5be9dcacb3"
+        private static let host: String = "https://api.themoviedb.org/3"
+        
+    }
+    
+    // MARK: -
+    
+}
