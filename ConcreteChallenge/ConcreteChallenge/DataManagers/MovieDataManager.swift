d80a3db89acb1585d1306b8972f12f6fb3fdf0d7
Heitor Ishihara <ishihara.heitor@gmail.com>
2018-11-12 23:21:36 -0200

    Creating data manager for Movies

--- ConcreteChallenge/ConcreteChallenge/DataManagers/MovieDataManager.swift
+++ ConcreteChallenge/ConcreteChallenge/DataManagers/MovieDataManager.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+enum RequestStatus {
+    case success
+    case failed
+}
+
+class MovieDataManager {
+    
+    // MARK: - Properties
+    static var movies: [Movie] = []
+    
+    static let getPopularMoviesURL = "https://api.themoviedb.org/3/movie/popular?api_key=0aa2fda064d1eec9e68bccc4220ddf7b&language=en-US&page=1"
+    
+    // MARK: - Functions
+    static func fetchPopularMovies(completion: @escaping () -> Void) {
+        guard let popularMoviesURL = URL(string: self.getPopularMoviesURL) else { return }
+        var request = URLRequest(url: popularMoviesURL)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if error == nil {
+                guard let data = data else { return }
+                
+                do {
+                    let decoder = JSONDecoder()
+                    
+                    let popularMoviesResponse = try decoder.decode(PopularMoviesResponse.self, from: data)
+                    self.movies = popularMoviesResponse.results
+
+                    completion()
+                } catch let decoderError {
+                    print("Error decoding json: ", decoderError)
+                }
+            } else {
+                print("Error requesting popular movies: ", error as Any)
+            }
+        }.resume()
+    }
+}
