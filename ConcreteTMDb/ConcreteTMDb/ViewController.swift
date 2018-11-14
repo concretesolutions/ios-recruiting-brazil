68ef210dc982b5432491a6d74befde289f3d0539
Pedro Ortiz <p.delrioortiz1997@gmail.com>
2018-11-14 11:22:11 -0200

    Additing project

---
+++ ConcreteTMDb/ConcreteTMDb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcreteTMDb
+//
+//  Created by Pedro Del Rio Ortiz on 14/11/18.
+//  Copyright © 2018 Pedro Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
