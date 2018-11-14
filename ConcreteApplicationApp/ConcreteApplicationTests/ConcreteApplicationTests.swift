b417e0883bc22e7de691bb0522db74687c4a9943
brunoCruz <brunohmcruz@icloud.com>
2018-11-14 16:29:36 -0200

    added project

---
+++ ConcreteApplicationApp/ConcreteApplicationTests/ConcreteApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  ConcreteApplicationTests.swift
+//  ConcreteApplicationTests
+//
+//  Created by Bruno Cruz on 14/11/18.
+//  Copyright © 2018 Bruno Cruz. All rights reserved.
+//
+
+import XCTest
+@testable import ConcreteApplication
+
+class ConcreteApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
