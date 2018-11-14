7a3bc0d7ea4f047a8f837fd83f70aba1254433f0
adannsergio <adannsergio@gmail.com>
2018-11-14 14:05:34 -0200

    Added MovieDescription VC

---
+++ Movs/Movs/Presentation/Controller/MovieDescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieDescriptionViewController.swift
+//  Movs
+//
+//  Created by Adann Simões on 14/11/18.
+//  Copyright © 2018 SAS. All rights reserved.
+//
+
+import UIKit
+
+class MovieDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
