6f16c89a0c687ef69b5b9704ac147355ba24214d
Heitor Ishihara <ishihara.heitor@gmail.com>
2018-11-15 04:31:01 -0200

    Creating datamanager to fetch genres

--- ConcreteChallenge/ConcreteChallenge/DataManagers/MovieDataManager.swift
+++ ConcreteChallenge/ConcreteChallenge/DataManagers/MovieDataManager.swift
@@ -12,9 +12,11 @@ class MovieDataManager {
     
     // MARK: - Properties
     static var movies: [Movie] = []
+    static var genres: [Genre] = []
     
     // The api_key should be into the info.plist but its here for easy testing
     static let getPopularMoviesURL = "https://api.themoviedb.org/3/movie/popular?api_key=0aa2fda064d1eec9e68bccc4220ddf7b&language=en-US&page=1"
+    static let getGenresURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=0aa2fda064d1eec9e68bccc4220ddf7b&language=en-US"
     
     // MARK: - Functions
     static func fetchPopularMovies(completion: @escaping () -> Void) {
@@ -46,6 +48,29 @@ class MovieDataManager {
     }
     
     static func fetchGenres() {
-
+        guard let genresURL = URL(string: self.getGenresURL) else { return }
+        var request = URLRequest(url: genresURL)
+        request.httpMethod = "GET"
+        
+        // Making GET api request
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if error == nil {
+                guard let data = data else { return }
+                
+                do {
+                    let decoder = JSONDecoder()
+                    
+                    // Decode it into Genres
+                    let genresResponse = try decoder.decode(GenresResponse.self, from: data)
+                    self.genres = genresResponse.genres
+                    
+                } catch let decoderError {
+                    print("Error decoding json: ", decoderError)
+                }
+            } else {
+                print("Error requesting genres: ", error as Any)
+            }
+            }.resume()
     }
 }
