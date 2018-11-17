1d8c69e9304d913eca0f40bd3cf314d0fef4dd4b
adannsergio <adannsergio@gmail.com>
2018-11-16 23:59:41 -0200

    Changed for a dynamic endpoint request

--- Movs/Movs/Business/DAO/PopularMovieDAO.swift
+++ Movs/Movs/Business/DAO/PopularMovieDAO.swift
@@ -11,18 +11,12 @@ import Alamofire
 
 struct PopularMovieDAO {
     static func getPopularMovie(page: Int, completionHandler: @escaping (PopularMovie?, Error?) -> Void) {
-        let endpoint = "\(APIRoute.Base.rawValue + APIRoute.PopularMovie.rawValue)"
-        let params: Parameters = [
-            "api_key": APIKey.Key.rawValue,
-            "language": Language.Portuguese.rawValue,
-            "page": page
-        ]
-
-        let teste = "https://api.themoviedb.org/3/movie/popular?api_key=a6d13c5f45d8dfa6cf3ff4334863c9a1&language=pt-BR&page=1"
+        //swiftlint:disable line_length
+        let endpoint = "\(APIRoute.Base.rawValue + APIRoute.PopularMovie.rawValue)?api_key=\(APIKey.Key.rawValue)&language=\(Language.Portuguese.rawValue)&page=\(page)"
+        //swiftlint:enable line_length
         
-        Alamofire.request(teste,
-                          method: .get,
-                          parameters: params).responseJSON { response in
+        Alamofire.request(endpoint,
+                          method: .get).responseJSON { response in
             switch response.result {
             case .success:
                 do {
