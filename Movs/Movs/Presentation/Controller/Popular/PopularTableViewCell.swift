87876d018a5318956d01fe5cdabefeaac4ba3ccd
adannsergio <adannsergio@gmail.com>
2018-11-17 01:46:46 -0200

    Setting up tableview data source: get first 20 results from movie/popular (first page), with title, rate average movie and it popular ranking position

---
+++ Movs/Movs/Presentation/Controller/Popular/PopularTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  PopularTableViewCell.swift
+//  Movs
+//
+//  Created by Adann Simões on 17/11/18.
+//  Copyright © 2018 SAS. All rights reserved.
+//
+
+import UIKit
+
+class PopularTableViewCell: UITableViewCell {
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var voteAverageLabel: UILabel!
+    @IBOutlet weak var favoriteIndicatorImage: UIImageView!
+    @IBOutlet weak var popularRankingLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setData(data: Results, popularRanking: Int, isFavorite: Bool) {
+        movieTitleLabel.text = data.title
+        voteAverageLabel.text = "\(data.voteAverage ?? 0.0) de 10"
+        if isFavorite {
+            favoriteIndicatorImage.image = UIImage(named: "@icons-favoriteSelected")
+        } else {
+            favoriteIndicatorImage.image = UIImage(named: "@icons-favoriteUnselected")
+        }
+        popularRankingLabel.text = String(popularRanking)
+    }
+
+}
--- Movs/Movs/Presentation/Controller/Popular/PopularViewController.swift
+++ Movs/Movs/Presentation/Controller/Popular/PopularViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 class PopularViewController: UITableViewController {
-    // MARK: Behavior IBOutlets
+    // MARK: Behavior View IBOutlets
     @IBOutlet var emptySearchView: UIView!
     @IBOutlet var genericErrorView: UIView!
     @IBOutlet var loadingView: UIView!
-    
+
     // MARK: Class Attributes
     var popularMovie: PopularMovie?
     var behavior: Behavior = .LoadingView {
@@ -21,6 +21,7 @@ class PopularViewController: UITableViewController {
             self.tableView.reloadData()
         }
     }
+    let popularMovieCellIdentifier = "popularCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +29,7 @@ class PopularViewController: UITableViewController {
     }
     
     func initialSetup() {
-        fetchMovieData(page: 1) { (data) -> Void in
+        fetchPopularMovieData(page: 1) { (data) -> Void in
             if data != nil {
                 self.popularMovie = data
                 self.setBehavior(newBehavior: .PopularMovies)
@@ -45,64 +46,24 @@ class PopularViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        if let numberOfRows = popularMovie?.results?.count {
+            return numberOfRows
+        }
         return 0
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: popularMovieCellIdentifier, for: indexPath) as? PopularTableViewCell
+        if let data = popularMovie?.results?[indexPath.row] {
+            cell?.setData(data: data, popularRanking: (indexPath.row + 1), isFavorite: false)
+        }
+        
+        return cell!
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(200.0)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
@@ -126,13 +87,12 @@ extension PopularViewController {
     }
 }
 
-// MARK: Service setup
+// MARK: Service call
 extension PopularViewController {
-    func fetchMovieData(page: Int, completionHandler: @escaping (PopularMovie?) -> Void) {
+    func fetchPopularMovieData(page: Int, completionHandler: @escaping (PopularMovie?) -> Void) {
         setBehavior(newBehavior: .LoadingView)
         PopularMovieServices.getPopularMovie(page: page) { (data, _) in
             if data != nil {
-                print(data?.totalPages)
                 completionHandler(data)
             } else {
                 self.setBehavior(newBehavior: .GenericError)
