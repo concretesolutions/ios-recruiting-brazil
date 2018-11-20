3a792f191fef7954a4bc28e09b8448ea9a96f764
rsg73626 <renan.rsg@hotmail.com>
2018-11-20 15:01:41 -0200

    Setting root view controller programmatically in app delegate to Movies module.

--- Movies/Movies/Application/AppDelegate.swift
+++ Movies/Movies/Application/AppDelegate.swift
@@ -14,9 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = MoviesRouter.assembleModule()
+        
         return true
     }
 
