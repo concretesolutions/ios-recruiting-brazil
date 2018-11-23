86eaa4d2ecdfd0062cc3688da2fe23c357545991
rsg73626 <renan.rsg@hotmail.com>
2018-11-23 03:57:21 -0200

    Implementing Presenter layer.

--- Movies/Movies/Modules/Favorites/Presenter/FavoritesPresenter.swift
+++ Movies/Movies/Modules/Favorites/Presenter/FavoritesPresenter.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+class FavoritesPresenter: FavoritesPresentation, FavoritesInteractorOutput {
+    
+    // MARK: - Properties
+    
+    weak var view: FavoritesView?
+    var router: FavoritesWireframe!
+    var interactor: FavoritesUseCase!
+    
+    // MARK: - FavoritesPresentation protocol functions
+    
+    func viewDidLoad() {
+        self.interactor.readFavoriteMovies()
+    }
+    
+    func didSelect(movie: Movie) {
+        self.router.presentMovieDetailsFor(movie)
+    }
+    
+    func didUnfavorite(movie: Movie) {
+        self.interactor.unfavorite(movie: movie)
+    }
+    
+    func didTapRemoveFilterButton() {
+        self.view?.hideRemoveFilterButton()
+        self.interactor.removeFilters()
+        self.interactor.readFavoriteMovies()
+    }
+    
+    func didSearchMovies(withTitle title: String) {
+        self.interactor.searchMovies(withTitle: title)
+    }
+    
+    func didFinishSearch() {
+        self.interactor.readFavoriteMovies()
+    }
+    
+    func didTapFilterButton() {
+        self.router.presentFiltersView()
+    }
+    
+    // MARK: - FavoritesInteractorOutput protocol functions
+    
+    func didRead(movies: [Movie]) {
+        self.view?.present(movies: movies)
+    }
+    
+    
+}
