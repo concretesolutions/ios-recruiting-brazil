65875758a729f816fe25292e5d6dc72b2ecaaec3
rsg73626 <renan.rsg@hotmail.com>
2018-11-23 20:43:49 -0200

    Creating tabbar module and class.

---
+++ Movies/Movies/Modules/Tabbar/MovsTabbarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovsTabbarViewController.swift
+//  Movies
+//
+//  Created by Renan Germano on 23/11/2018.
+//  Copyright © 2018 Renan Germano. All rights reserved.
+//
+
+import UIKit
+
+class MovsTabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
