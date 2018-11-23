b58d56362a9bbdf06f4e0867802c0f7523f6c8e0
rsg73626 <renan.rsg@hotmail.com>
2018-11-23 20:50:57 -0200

    Implementing MovsTabbarController.

--- Movies/Movies/Modules/Tabbar/MovsTabbarViewController.swift
+++ Movies/Movies/Modules/Tabbar/MovsTabbarViewController.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class MovsTabbarViewController: UITabBarController {
+    
+    // MARK: - Properties
+    
+    private var moviesPresenter: MoviesPresentation?
+    private var favoritesPresenter: FavoritesPresentation?
 
+    // MARK: - Life cicle functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let moviesView = MoviesRouter.assembleModule()
+        let favoritesView = FavoritesRouter.assembleModule()
+        
+        if let mv = moviesView as? MoviesView,
+           let fv = favoritesView as? FavoritesView {
+            self.moviesPresenter = mv.presenter
+            self.favoritesPresenter = fv.presenter
+        }
+        
+        self.viewControllers = [moviesView, favoritesView]
+        
+        self.selectedIndex = 0
+        
+    }
+    
+    // MARK: - Tabbar functions
+    // TODO: - Make scroll to the top of view when the user taps in the icon of an already selected tab bar icon
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
     }
 
 }
