117eb25c95f18478b07ecb85fa8b85a28bf47fbd
rsg73626 <renan.rsg@hotmail.com>
2018-11-23 03:39:37 -0200

    Integrating all files to implement FavoritesViewController.

--- Movies/Movies/Modules/Favorites/View/FavoritesViewController.swift
+++ Movies/Movies/Modules/Favorites/View/FavoritesViewController.swift
@@ -13,25 +13,46 @@ class FavoritesViewController: UIViewController, FavoritesView {
     // MARK: - Outlets
     
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var favorites: UITableView!
+    @IBOutlet weak var favorites: FavoritesTableView!
     
     // MARK: - Properties
     
     var presenter: FavoritesPresentation!
     private var removeFilterButton: UIButton!
+    private var delegate: FavoritesTVDelegate!
+    private var dataSource: FavoritesTVDataSource!
+    private var searchDelegate: FavoritesSearchBarDelegate!
     
     // MARK: - Life cicle functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.delegate = FavoritesTVDelegate(presenter: self.presenter)
+        self.favorites.delegate = self.delegate
+        
+        self.dataSource = FavoritesTVDataSource(tableView: self.favorites, presenter: self.presenter)
+        self.favorites.dataSource = self.dataSource
+        
+        self.searchDelegate = FavoritesSearchBarDelegate(presenter: self.presenter, viewController: self)
+        self.searchBar.delegate = self.searchDelegate
+        
+        self.setUpFilterButton()
+        self.setUpRemoveFilterButton()
 
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Favorites"
+        self.showActivityIndicator()
+        
+        self.presenter.viewDidLoad()
     }
     
     // MARK: - FavoritesView protocol functions
     
     func present(movies: [Movie]) {
-        
+        DispatchQueue.main.async {
+            self.hideRemoveFilterButton()
+            self.dataSource.update(movies: movies)
+        }
     }
     
     func showRemoveFilterButton() {
@@ -50,6 +71,19 @@ class FavoritesViewController: UIViewController, FavoritesView {
     
     @objc func didTapHideFilterButton() {
         
+    }
+    
+    // MARK: - Aux functions
+    
+    private func setUpFilterButton() {
+        let filterButton = UIBarButtonItem(image: UIImage(named: "FilterIcon"), style: .plain, target: self, action: #selector(self.didTapFilterButton))
+        self.navigationController?.navigationItem.rightBarButtonItem = filterButton
+    }
+    
+    private func setUpRemoveFilterButton() {
+        self.removeFilterButton = UIButton(type: .system)
+        self.removeFilterButton.setTitle("remove filters", for: .normal)
+        self.removeFilterButton.addTarget(self, action: #selector(self.didTapHideFilterButton), for: .touchUpInside)
     }
     
 
