03ba060338f1b5670970ae225ef2c47edc78b45a
rsg73626 <renan.rsg@hotmail.com>
2018-11-24 02:31:58 -0200

    Creating and implementing Router layer file.

--- Movies/Movies/Modules/MovieDetail/Router/MovieDetailRouter.swift
+++ Movies/Movies/Modules/MovieDetail/Router/MovieDetailRouter.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2018 Renan Germano. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MovieDetailRouter: MovieDetailWireframe {
+    
+    // MARK: - Properties
+    
+    var view: UIViewController?
+    
+    // MARK: - MoviewDetailWireframe protocol functions
+    
+    static func assembleModule(withMovie movie: Movie) -> UIViewController {
+        
+        let presenter = MovieDetailPresenter()
+        let interactor = MovieDetailInteractor()
+        let router = MovieDetailRouter()
+        
+        presenter.interactor = interactor
+        presenter.router = router
+        
+        interactor.output = presenter
+        interactor.movie = movie
+        
+        let storyboard = UIStoryboard(name: "MovieDetail", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "MovieDetail")
+        
+        router.view = viewController
+        
+        if let movieDetailViewController = viewController as? MovieDetailView {
+            movieDetailViewController.presenter = presenter
+            presenter.view = movieDetailViewController
+        }
+        
+        return viewController
+        
+    }
+    
+}
