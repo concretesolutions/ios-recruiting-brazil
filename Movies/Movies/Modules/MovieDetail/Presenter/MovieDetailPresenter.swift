da40162e405bfcfbbbc17cfa17592f07152e524a
rsg73626 <renan.rsg@hotmail.com>
2018-11-24 02:21:35 -0200

    Creating and implementing Presenter layer files.

--- Movies/Movies/Modules/MovieDetail/Presenter/MovieDetailPresenter.swift
+++ Movies/Movies/Modules/MovieDetail/Presenter/MovieDetailPresenter.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+class MovieDetailPresenter: MovieDetailPresentation, MovieDetailInteractorOutput {
+    
+    // MARK: - Properties
+    
+    weak var view: MovieDetailView?
+    var router: MovieDetailWireframe!
+    var interactor: MovieDetailUseCase!
+    
+    // MARK: - MovieDetailPresentation protocol functions
+    
+    func viewDidLoad() {
+        self.interactor.getMovie()
+    }
+    
+    func didTapFavoriteButton(forMovie movie: Movie) {
+        if movie.isFavorite {
+            self.interactor.unfavorite(movie: movie)
+        } else {
+            self.interactor.favorite(movie: movie)
+        }
+    }
+    
+    // MARK: - MovieDetailInteractorOutput protocol functions
+    
+    func didGet(movie: Movie) {
+        self.view?.present(movie: movie)
+    }
+    
+}
