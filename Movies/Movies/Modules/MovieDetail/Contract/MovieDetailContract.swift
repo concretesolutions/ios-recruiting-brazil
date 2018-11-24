023dac87a5a97a769c10c86b32045738e6a5fd61
rsg73626 <renan.rsg@hotmail.com>
2018-11-24 01:29:06 -0200

    Creating Movie Detail contract layer (declaring all protocols).

---
+++ Movies/Movies/Modules/MovieDetail/Contract/MovieDetailContract.swift
@@ -0,0 +1,39 @@
+//
+//  MovieDetailContract.swift
+//  Movies
+//
+//  Created by Renan Germano on 24/11/2018.
+//  Copyright © 2018 Renan Germano. All rights reserved.
+//
+
+import UIKit
+
+protocol MovieDetailView: class {
+    
+    
+    
+}
+
+protocol MovieDetailPresentation {
+    
+    
+    
+}
+
+protocol MovieDetailUseCase {
+    
+    
+    
+}
+
+protocol MovieDetailInteractorOutput {
+    
+    
+    
+}
+
+protocol MovieDetailWireframe {
+    
+    
+    
+}
