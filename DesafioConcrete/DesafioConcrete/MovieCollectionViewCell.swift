e688812c1b320763eac86ae3762d4f20e04be9ea
imvm <imvm@cin.ufpe.br>
2018-12-17 19:43:52 -0300

    Add movie grid cell

---
+++ DesafioConcrete/DesafioConcrete/MovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionViewCell.swift
+//  DesafioConcrete
+//
+//  Created by Ian Manor on 17/12/18.
+//  Copyright © 2018 Ian. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    @IBOutlet weak var favoriteIconImageView: UIImageView!
+    
+}
---
+++ DesafioConcrete/DesafioConcrete/MovieGridCollectionViewController.swift
@@ -0,0 +1,108 @@
+//
+//  MovieGridCollectionViewController.swift
+//  DesafioConcrete
+//
+//  Created by Ian Manor on 17/12/18.
+//  Copyright © 2018 Ian. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "MovieCell"
+
+class MovieGridCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let movies = ["Thor", "Iron Man", "Avengers", "Roma", "Call me by your name"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return movies.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MovieCollectionViewCell
+    
+        // Configure the cell
+        cell.movieTitleLabel.text = movies[indexPath.row]
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // Screenshot cell width: 501, height: 618.
+        // Screenshot device width: 1125, height: 2001.
+        
+        let widthPercentageOfScreen: CGFloat = 501 / 1125
+        let cellWidth = self.collectionView.frame.width * widthPercentageOfScreen
+        
+        let heightPercentageOfScreen: CGFloat = 618 / 2001
+        let cellHeight = self.collectionView.frame.height * heightPercentageOfScreen
+        
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 16, bottom: 10, right: 16)
+    }
+
+}
