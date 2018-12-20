17d74b5af4942ac4dcae191ea4af3f8dd823d206
brunoCruz <brunohmcruz@icloud.com>
2018-12-20 14:25:22 -0200

    Created CollectionView

--- ConcreteApplicationApp/Datasources/MoviesGridCollectionDataSource.swift
+++ ConcreteApplicationApp/Datasources/MoviesGridCollectionDataSource.swift
@@ -29,6 +29,20 @@ final class MoviesGridCollectionDataSource: NSObject, UICollectionViewDataSource
         cell.setup(movie: movies[indexPath.row])
         return cell
     }
+}
+
+class MoviesGridCollectionDelegate: NSObject, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
+    
+    let movies:[Movie]!
+    
+    init(movies: [Movie]){
+        self.movies = movies
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width * 0.4
+        return CGSize(width: width, height: width)
+    }
     
     
 }
--- ConcreteApplicationApp/View/MoviesGrid/MoviesGridViewController.swift
+++ ConcreteApplicationApp/View/MoviesGrid/MoviesGridViewController.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
+import SnapKit
 
+//FIXME:- change protocol file
 protocol CodeView{
     func buildViewHierarchy()
     func setupConstraints()
@@ -28,31 +30,70 @@ extension CodeView{
 
 class MoviesGridViewController: UIViewController {
 
+    //CollectionView
     let collectionView = MoviesGridCollectionView()
-//    let collectionViewDataSource: MoviesGridCollectionDataSource?
+    var collectionViewDataSource: MoviesGridCollectionDataSource?
+    var collectionViewDelegate: MoviesGridCollectionDelegate?
     //FIXME:- create collection delegate and setup collection in willAppear
+    //TMDB API
+    let tmdb = TMDBManager()
+    //Properties
+    var movies:[Movie] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupView()
+        tmdb.getPopularMovies(page: 1) { (result) in
+            switch result{
+            case .success(let movies):
+                self.handleFetchOf(movies: movies)
+            case .error:
+            print("error")
+            //FIXME:- display error
+            }
+        }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    func handleFetchOf(movies:[Movie]){
+        self.setupCollectionView(with: movies)
+    }
+    
+    
+    func setupCollectionView(with movies: [Movie]){
+        
+        self.collectionView.isHidden = false
+        collectionViewDataSource = MoviesGridCollectionDataSource(movies: movies, collectionView: self.collectionView)
+        self.collectionView.dataSource = collectionViewDataSource
+        collectionViewDelegate = MoviesGridCollectionDelegate(movies: movies)
+        self.collectionView.delegate = collectionViewDelegate
+        self.collectionView.reloadData()
+        
+    }
+    
 }
+
 
 extension MoviesGridViewController: CodeView{
     func buildViewHierarchy() {
-        //
+        view.addSubview(collectionView)
     }
     
     func setupConstraints() {
-        //
+        collectionView.snp.makeConstraints { (make) in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
     }
     
     func setupAdditionalConfiguration() {
         //
     }
-    
-    func setupView() {
-        //
-    }
-    
     
 }
