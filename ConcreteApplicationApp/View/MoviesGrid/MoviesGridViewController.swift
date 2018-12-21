b7426233369265f98d710cf0acac3290df90ff0a
brunoCruz <brunohmcruz@icloud.com>
2018-12-20 23:37:04 -0200

    Created States for UI and implemented loadingState with activityIndicator

--- ConcreteApplicationApp/View/MoviesGrid/MoviesGridViewController.swift
+++ ConcreteApplicationApp/View/MoviesGrid/MoviesGridViewController.swift
@@ -15,20 +15,51 @@ class MoviesGridViewController: UIViewController {
     let collectionView = MoviesGridCollectionView()
     var collectionViewDataSource: MoviesGridCollectionDataSource?
     var collectionViewDelegate: MoviesGridCollectionDelegate?
+    var activityIndicator = ActivityIndicator(frame: .zero)
     //TMDB API
     let tmdb = TMDBManager()
     //Properties
     var movies:[Movie] = []
     
+    //FIXME:- learn about fileprivate
+    
+    fileprivate enum LoadingState{
+        case loading
+        case ready
+    }
+    
+    fileprivate enum PresentationState{
+        case loadingContent
+        case displayingContent
+        case error
+    }
+    
+    fileprivate var loadingState: LoadingState = .ready {
+        didSet{
+            refreshLoading(state: loadingState)
+        }
+    }
+    
+    fileprivate var presentationState: PresentationState = .loadingContent{
+        didSet{
+            refreshUI(for: presentationState)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
+        
+        loadingState = .loading
+        presentationState = .loadingContent
         tmdb.getPopularMovies(page: 1) { (result) in
+            self.loadingState = .ready
             switch result{
             case .success(let movies):
                 self.handleFetchOf(movies: movies)
             case .error:
+                self.presentationState = .error
                 print("error")
                 //FIXME:- display error
             }
@@ -37,6 +68,7 @@ class MoviesGridViewController: UIViewController {
     
     func handleFetchOf(movies:[Movie]){
         self.setupCollectionView(with: movies)
+        self.presentationState = .displayingContent
     }
     
     
@@ -51,10 +83,12 @@ class MoviesGridViewController: UIViewController {
     
 }
 
-
+//MARK:- CodeView protocol
 extension MoviesGridViewController: CodeView{
+    
     func buildViewHierarchy() {
         view.addSubview(collectionView)
+        view.addSubview(activityIndicator)
     }
     
     func setupConstraints() {
@@ -64,8 +98,41 @@ extension MoviesGridViewController: CodeView{
             make.top.equalToSuperview()
             make.bottom.equalToSuperview()
         }
+        activityIndicator.snp.makeConstraints { (make) in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+}
+
+//MARK:- handling with UI changings
+
+extension MoviesGridViewController{
+    
+    fileprivate func refreshLoading(state: LoadingState){
+        switch state{
+        case .loading:
+            activityIndicator.startAnimating()
+            print("start activity Indicator")
+        case .ready:
+            activityIndicator.stopAnimating()
+            print("stop activity Indicator")
+        }
     }
     
-    func setupAdditionalConfiguration() {}
+    fileprivate func refreshUI(for presentationState: PresentationState){
+        switch presentationState{
+        case .loadingContent:
+            print("display loading state")
+            collectionView.isHidden = true
+            activityIndicator.isHidden = false
+        case .displayingContent:
+            print("display content")
+        case .error:
+            print("error state")
+        }
+    }
     
 }
