1501362838c0d3ae8db228d0a59a5b355821e77a
Bruno Muniz <bruno.muniz@supercharge.io>
2018-12-26 22:41:01 -0300

    Add kingfisher pod

---
+++ movs/movs/Controllers/MovieListCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieListCell.swift
+//  movs
+//
+//  Created by Bruno Muniz Azevedo Filho on 26/12/18.
+//  Copyright © 2018 bmaf. All rights reserved.
+//
+
+import UIKit
+
+final class MovieListCell: UICollectionViewCell {
+    
+}
