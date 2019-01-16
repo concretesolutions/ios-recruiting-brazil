404aaba16023bac48a11b6cf9013ce8068b78ebc
vinicius emanuel <vncemanuel@gmail.com>
2019-01-16 19:06:03 -0300

    assets, slash screen, basico da navegação.

---
+++ Movs/Movs/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movs
+//
+//  Created by vinicius emanuel on 16/01/19.
+//  Copyright © 2019 vinicius emanuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
