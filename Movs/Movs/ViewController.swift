bac1278188b4f057a861f560d1f06946dbbaea1b
Franclin Cabral <franclincabral@gmail.com>
2019-01-17 22:13:35 -0300

    Criando projeto com gitignore

---
+++ Movs/Movs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movs
+//
+//  Created by Franclin Cabral on 1/17/19.
+//  Copyright © 2019 franclin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
