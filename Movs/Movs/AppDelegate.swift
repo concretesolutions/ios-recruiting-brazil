558e74be914d2838e5f07a378d569b998d0805bb
Filipe Nogueira Jordão <filipejrd@hotmail.com>
2019-01-22 19:05:28 -0300

    Adding base project

---
+++ Movs/Movs/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Movs
+//
+//  Created by Filipe Jordão on 22/01/19.
+//  Copyright © 2019 Filipe Jordão. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ Movs/Movs/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Movs
+//
+//  Created by Filipe Jordão on 22/01/19.
+//  Copyright © 2019 Filipe Jordão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
