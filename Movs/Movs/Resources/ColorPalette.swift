bf9b8f8bb58bb8f6e609307f0305d17d5f7b5d78
Brendoon Ryos <bryos@MBP-de-Brendoon.lan>
2019-02-07 03:23:46 -0200

    Adding color palette

---
+++ Movs/Movs/Resources/ColorPalette.swift
@@ -0,0 +1,19 @@
+//
+//  ColorPalette.swift
+//  Movs
+//
+//  Created by Brendoon Ryos on 25/01/19.
+//  Copyright © 2019 Brendoon Ryos. All rights reserved.
+//
+
+import UIKit
+
+enum ColorPalette {
+  static let white =  UIColor(hex: "FAFAFA")
+  
+  static let yellow =  UIColor(hex: "F3B62A")
+  
+  static let black =  UIColor(hex: "474747")
+  
+  static let red = UIColor(hex: "931621")
+}
