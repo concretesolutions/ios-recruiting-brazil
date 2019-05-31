2d43f176834c7d0404df40fa088f0894bd904fbd
Tomaz <tomazcorreasilva@gmail.com>
2019-05-31 19:26:46 -0300

    commit inicial do projeto

---
+++ Cineasta/Cineasta/Scene/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Cineasta
+//
+//  Created by Tomaz Correa on 31/05/19.
+//  Copyright © 2019 TCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Cineasta/Pods/Target Support Files/Pods-Cineasta/Pods-Cineasta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cineasta : NSObject
+@end
+@implementation PodsDummy_Pods_Cineasta
+@end
