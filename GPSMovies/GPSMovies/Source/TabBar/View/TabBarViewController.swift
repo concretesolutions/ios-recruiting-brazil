630edf600a0f838ac09c69447f70639e3fcd4771
Gilson Santos <gilsonsantosti@gmail.com>
2019-06-01 10:25:14 -0300

    Inicio  da criacao da arquitetura e das pastas do projeto.

---
+++ GPSMovies/GPSMovies/Source/TabBar/View/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPSMovies
+//
+//  Created by Gilson Santos on 01/06/19.
+//  Copyright © 2019 Gilson Santos. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
