5759efef1b5f2a47ef3318a9d44fb38830460744
Gabriel Henrique <gabhenrique0@gmail.com>
2019-06-02 16:43:14 -0300

    Create a file to storage colors inside a Struct

---
+++ movies-app/MoviesApp/MoviesApp/Supporting Files/Constants/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  MoviesApp
+//
+//  Created by Gabriel Pereira on 02/06/19.
+//  Copyright © 2019 Gabriel Pereira. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let yellow = UIColor(red:0.97, green:0.81, blue:0.36, alpha:1.0)
+}
