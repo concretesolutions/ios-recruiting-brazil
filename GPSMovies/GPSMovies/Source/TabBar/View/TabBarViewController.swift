752c51effc5f41e169c03b94abcd7240f13b150f
Gilson Santos <gilsonsantosti@gmail.com>
2019-06-04 21:16:42 -0300

    Ajustes de layout na tela de favoritos

--- GPSMovies/GPSMovies/Source/TabBar/View/TabBarViewController.swift
+++ GPSMovies/GPSMovies/Source/TabBar/View/TabBarViewController.swift
@@ -28,6 +28,7 @@ extension TabBarViewController {
         self.delegate = self
         self.presenter = TabBarPresenter(viewDelegate: self)
         self.presenter.getTabBarItens()
+        self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
 }
 
