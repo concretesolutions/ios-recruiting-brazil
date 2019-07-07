f28b4e860725f4fe9d3b69523e11357ea758aa2f
Gabriel Sória <gabrielss.rock@me.com>
2019-07-07 10:01:57 -0300

    Adding a class for popular movies collection view.

---
+++ Test-MovieDB/PopularMovies/PopularMoviesCollectionView.swift
@@ -0,0 +1,15 @@
+//
+//  PopularMoviesCollectionView.swift
+//  Test-MovieDB
+//
+//  Created by Gabriel Soria Souza on 07/07/19.
+//  Copyright © 2019 Gabriel Sória Souza. All rights reserved.
+//
+
+import UIKit
+
+class PopularMoviesCollectionView: UICollectionView {
+
+  
+
+}
