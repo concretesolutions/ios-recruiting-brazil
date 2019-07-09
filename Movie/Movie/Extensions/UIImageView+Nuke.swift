42b64689cdcb06c308e02b08a3b1ffeacf575bae
Elton Santana <faletaelton@gmail.com>
2019-07-09 14:13:57 -0300

    Image download method with nuke

--- Movie/Movie/Extensions/UIImageView+Nuke.swift
+++ Movie/Movie/Extensions/UIImageView+Nuke.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import UIKit
+import Nuke
+
+extension UIImageView {
+    func loadImageFrom(_ path: String) {
+        let url = URL(string: "https://image.tmdb.org/t/p/w342" + path)!
+
+        ImagePipeline.shared.loadImage(with: url, progress: { (response, _, _) in
+            guard let response = response else {
+                return
+            }
+            
+            self.image = response.image
+            
+        }) { (response, error) in
+            //
+        }
+    }
+ }
