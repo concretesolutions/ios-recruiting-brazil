6b4f204d15cb6871cfafabc31d4d511229250959
Elton Santana <faletaelton@gmail.com>
2019-07-09 08:03:23 -0300

    Added initial project
    with .gitignore

---
+++ Movie/Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Elton Santana on 09/07/19.
+//  Copyright © 2019 Memo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
