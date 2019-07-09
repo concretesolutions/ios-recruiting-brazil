1e1e1b60823249a4f43334910924b70b4966e929
Elton Santana <faletaelton@gmail.com>
2019-07-09 17:07:07 -0300

    Copied assets from forked repo

---
+++ Movie/Movie/UserInterface/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  Movie
+//
+//  Created by Elton Santana on 09/07/19.
+//  Copyright © 2019 Memo. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
