64c24d95e35e94f7626eca9be05f25ab59fc9c62
Gustavo Pereira Teixeira Quenca <gustavo.quenca@mc1.com.br>
2019-07-19 12:49:42 -0300

    Fix Loading XIB

---
+++ Movie/Movie/View/LoadingCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingCollectionViewCell.swift
+//  Movie
+//
+//  Created by Gustavo Pereira Teixeira Quenca on 19/07/19.
+//  Copyright © 2019 Gustavo Pereira Teixeira Quenca. All rights reserved.
+//
+
+import UIKit
+
+class LoadingCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
