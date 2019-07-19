f133f925b9c66e6a65a5e0fbcf4f2f3a490e3eaa
Gustavo Pereira Teixeira Quenca <gustavo.quenca@mc1.com.br>
2019-07-19 12:44:40 -0300

    Nothing Found Xib

---
+++ Movie/Movie/View/NothingFoundCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NothingFoundCollectionViewCell.swift
+//  Movie
+//
+//  Created by Gustavo Pereira Teixeira Quenca on 19/07/19.
+//  Copyright © 2019 Gustavo Pereira Teixeira Quenca. All rights reserved.
+//
+
+import UIKit
+
+class NothingFoundCollectionViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
