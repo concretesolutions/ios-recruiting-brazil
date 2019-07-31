a75bb3a26e0f68e4ad2938cc18dfc67538018435
Henrique Barbosa <henriqbarbosa@icloud.com>
2019-07-31 18:17:35 -0300

    Init and add assets

---
+++ Concrete Movies/Concrete Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concrete Movies
+//
+//  Created by Henrique Barbosa on 31/07/19.
+//  Copyright © 2019 Henrique Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
