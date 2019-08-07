7f803eded4effc3f006e62a80357724dd22de556
Henrique Campos de Freitas <henriquecfreitas@hotmail.com.br>
2019-08-07 12:27:57 -0300

    Creating project from Xcode

---
+++ xcodeProject/Movs/MovsTests/MovsTests.swift
@@ -0,0 +1,34 @@
+//
+//  MovsTests.swift
+//  MovsTests
+//
+//  Created by Henrique Campos de Freitas on 07/08/19.
+//  Copyright © 2019 Henrique Campos de Freitas. All rights reserved.
+//
+
+import XCTest
+@testable import Movs
+
+class MovsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
