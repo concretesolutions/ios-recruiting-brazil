21988dd553bfd5c3e4fd6da14d694f7a01027277
Leonardo Viana <leonardovianaamorim@gmail.com>
2019-08-11 09:45:23 -0300

    Adicionado teste de interface

---
+++ SearchMovies/SearchMoviesUITests/SearchMoviesUITests.swift
@@ -0,0 +1,34 @@
+//
+//  SearchMoviesUITests.swift
+//  SearchMoviesUITests
+//
+//  Created by Leonardo Viana on 11/08/19.
+//  Copyright © 2019 Leonardo. All rights reserved.
+//
+
+import XCTest
+
+class SearchMoviesUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testLoadMovies() {
+        XCUIApplication().children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .collectionView).element.swipeUp()
+         
+    }
+
+}
