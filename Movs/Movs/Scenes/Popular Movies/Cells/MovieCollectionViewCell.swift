b3d1520bf9ad3295249cbab6f6d0156cb64b7ec5
Tiago Chaves <tchaves74@gmail.com>
2019-08-12 00:15:08 -0300

    Start popular movie list scene
    - Add icons for tab, filter and favorite;
    - Create Popular Movies Scene;
    - Create movie list layout on storyboard;
    - Create a movie collection cell.

---
+++ Movs/Movs/Scenes/Popular Movies/Cells/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  Movs
+//
+//  Created by Tiago Chaves on 11/08/19.
+//  Copyright © 2019 Tiago Chaves. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
