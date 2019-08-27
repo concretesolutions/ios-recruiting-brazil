543f525064ded72b52825dade8f0a25b131a4ba1
Euclides <Euclides>
2019-08-27 14:59:16 -0300

    Criando cell e models

--- MovieApp/Model/Movie.swift
+++ MovieApp/Model/Movie.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+class Movie: Codable{
+    
+    var voteCount: Int
+    var idMovie: Int
+    var voteAverage: Float
+    var title: String
+    var popularity: Float
+    var posterPath: String
+    var genreIds: [Int]
+    var backdropPath: String
+    var adult: Bool
+    var overview: String
+    
+    init(voteCount: Int,idMovie: Int,voteAverage: Float,title: String,popularity:Float,posterPath:String,genreIds:[Int],backdropPath:String,adult:Bool,overview:String) {
+        
+        self.voteCount = voteCount
+        self.idMovie = idMovie
+        self.voteAverage = voteAverage
+        self.title = title
+        self.popularity = popularity
+        self.posterPath = posterPath
+        self.genreIds = genreIds
+        self.backdropPath = backdropPath
+        self.adult = adult
+        self.overview = overview
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case voteCount = "vote_count"
+        case idMovie = "id"
+        case voteAverage = "vote_average"
+        case title, popularity
+        case posterPath = "poster_path"
+        case genreIds = "genre_ids"
+        case backdropPath = "backdrop_path"
+        case adult, overview
+    }
+    
+}
--- MovieApp/View/Cell/HomeVC.swift
+++ MovieApp/View/Cell/HomeVC.swift
@@ -10,21 +10,16 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    @IBOutlet weak var topoIV: UIImageView!
+    @IBOutlet weak var titleLb: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
