e66b0ea4494926c6ba22d2e4d739e48eac95aaa1
Euclides <Euclides>
2019-08-27 14:58:56 -0300

    Criando cell e model

---
+++ MovieApp/Model/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  MovieApp
+//
+//  Created by Mac Pro on 27/08/19.
+//  Copyright © 2019 Mac Pro. All rights reserved.
+//
+
+import Foundation
---
+++ MovieApp/View/Cell/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  MovieApp
+//
+//  Created by Mac Pro on 27/08/19.
+//  Copyright © 2019 Mac Pro. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
