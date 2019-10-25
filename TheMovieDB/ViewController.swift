6c649a2b773d5bcf65d436933f19ea8c42e7e9e8
Ranieri Aguiar <ranieri.aguiar@trinitysolucoes.com>
2019-10-24 21:32:39 -0300

    Initial Commit

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Ranieri Aguiar on 24/10/19.
+//  Copyright © 2019 Ranieri Aguiar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
