dc5d147a997ed88ab8fba4f9cd2829e5926a151f
Bruno Barbosa <brunobt94@gmail.com>
2019-10-27 18:29:20 -0300

    AppColors UIColor extension

---
+++ Movs/Movs/Utils/Extensions/UIColor+AppColors.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+AppColors.swift
+//  Movs
+//
+//  Created by Bruno Barbosa on 27/10/19.
+//  Copyright © 2019 Bruno Barbosa. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var appYellow = UIColor(named: "yellow")
+    static var appDarkBlue = UIColor(named: "darkBlue")
+}
