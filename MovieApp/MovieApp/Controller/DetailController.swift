9b18754855a9654f31415afb9e82451022686338
Giuliano Accorsi <giulianoaccorsi@gmail.com>
2019-11-07 21:38:08 -0300

    terminei as controllers

---
+++ MovieApp/MovieApp/Controller/DetailController.swift
@@ -0,0 +1,68 @@
+//
+//  DetailController.swift
+//  MovieApp
+//
+//  Created by Giuliano Accorsi on 07/11/19.
+//  Copyright © 2019 Giuliano Accorsi. All rights reserved.
+//
+
+
+import Foundation
+
+class DetailController {
+    
+    var movie: Movie?
+    let dataManager: DataManager = DataManager()
+    
+    func saveMovie(movie: Movie) {
+        dataManager.loadMovie { (arrayCoreData) in
+            let arraySaved = arrayCoreData.filter{$0.id == Int64(movie.id)}
+            if arraySaved.count > 0 {
+                dataManager.delete(id: arraySaved.first!.objectID) { (success) in
+                    if success {
+                        print("Removeu")
+                    }
+                }
+            }else {
+                dataManager.saveMovie(movieToSave: movie, genres: dataManager.getGenreString(movie: movie))
+            }
+        }
+    }
+    
+    func isFavorite(movie: Movie) -> Bool {
+        var array:[MovieSave]  = []
+        dataManager.loadMovie { (arrayCoreData) in
+            array = arrayCoreData
+        }
+        let arraySaved = array.filter{$0.id == Int64(movie.id)}
+        return arraySaved.count > 0
+    }
+    
+    func dataFormatter(movie: Movie?, movieSave: MovieSave?) -> String {
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd"
+        
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "dd/MM/yyyy"
+        
+        if let movieSelected = movie, let date = dateFormatterGet.date(from: movieSelected.releaseDate) {
+            return dateFormatterPrint.string(from: date)
+        } else if let movieSelectedSave = movieSave, let date = dateFormatterGet.date(from: movieSelectedSave.releaseDate ?? "") {
+            return dateFormatterPrint.string(from: date)
+        } else {
+            print("There was an error decoding the string")
+            return ""
+        }
+    }
+    
+    func getGenre(movie: Movie?) -> String {
+        if let movieSelected = movie {
+            return dataManager.getGenreString(movie: movieSelected)
+        }
+        return ""
+    }
+    
+}
+
+
+
