d70c5012f763c8bf6abcf7fbadf83c93154d16f6
Lucas <lucas.rebelato@hotmail.com>
2019-11-26 14:32:52 -0300

    Initial Commit

---
+++ Desafio ConcreteTests/Desafio_ConcreteTests.swift
@@ -0,0 +1,34 @@
+//
+//  Desafio_ConcreteTests.swift
+//  Desafio ConcreteTests
+//
+//  Created by Lucas Rebelato on 26/11/19.
+//  Copyright © 2019 Lucas Rebelato. All rights reserved.
+//
+
+import XCTest
+@testable import Desafio_Concrete
+
+class Desafio_ConcreteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
