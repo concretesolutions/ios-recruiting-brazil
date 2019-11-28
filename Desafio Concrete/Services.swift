0aa5de2c51ad2bc18c1ffa48c8f8b327d57198c7
Lucas <lucas.rebelato@hotmail.com>
2019-11-28 19:37:50 -0300

    Adicionando arquivo Services.swift

---
+++ Desafio Concrete/Services.swift
@@ -0,0 +1,196 @@
+//
+//  Services.swift
+//  Desafio Concrete
+//
+//  Created by Lucas Rebelato on 28/11/19.
+//  Copyright © 2019 Lucas Rebelato. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AlamofireImage
+import Alamofire
+
+class RequestAPI {
+    func pegarFilmesPopulares(pagina: Int, completion: @escaping([Filme], String?) -> ()){
+
+        var listaFilmesObj: [Filme] = []
+
+        let url = "https://api.themoviedb.org/3/movie/popular?api_key=dcfdbbf8648cddebeb0decb1f27aca9a&language=pt-BR&page=\(pagina)"
+
+        let request = NSMutableURLRequest(url: NSURL(string: url)! as URL,
+                                            cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+
+        let dataTask = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                    
+            if let erro = error {
+                print(erro.localizedDescription)
+                completion([],"\(erro.localizedDescription)")
+            }
+                    
+            if let data = data {
+                do {
+                
+                    let json = try JSONDecoder().decode(Lista.self, from: data)
+                    
+                    let semaforo = DispatchSemaphore(value: 1)
+                
+                    if json.total_pages < pagina {
+                        semaforo.signal()
+                        completion(listaFilmesObj, nil)
+                    }
+            
+                    for result in json.results {
+        
+                        let _ = Filme(filmeDecodable: result) { (filme) in
+                            listaFilmesObj.append(filme)
+                            semaforo.signal()
+                        }
+                        
+                        semaforo.wait()
+                    }
+                
+                    completion(listaFilmesObj, nil)
+            
+                } catch {
+                    print(error)
+                    completion(listaFilmesObj, error.localizedDescription)
+                }
+            }
+        })
+        
+        dataTask.resume()
+    }
+   
+   func pegarFilmesPorID(id: Int, completion: @escaping(Filme?, String?) -> ()){
+ 
+       let url = "https://api.themoviedb.org/3/movie/\(id)?api_key=dcfdbbf8648cddebeb0decb1f27aca9a&language=pt-BR"
+       
+       let request = NSMutableURLRequest(url: NSURL(string: url)! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                                  timeoutInterval: 10.0)
+       request.httpMethod = "GET"
+
+       let dataTask = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                       
+           if let erro = error {
+               print(erro.localizedDescription)
+               completion(nil,nil)
+           }
+                       
+           if let data = data {
+               do {
+                   
+                   let json = try JSONDecoder().decode(FilmeDecodable.self, from: data)
+                   
+                   let _ = Filme(filmeDecodable: json) { (filme) in
+                       completion(filme,nil)
+                   }
+               
+               } catch {
+                   print(error)
+                   completion(nil,nil)
+               }
+           }
+       })
+       
+       dataTask.resume()
+   }
+   
+   func baixarPosterFilme(filme: FilmeDecodable, completion: @escaping(UIImage) -> ()){
+       
+       guard let url = filme.poster_path else {
+           completion(UIImage())
+           return
+       }
+       
+       Alamofire.request("http://image.tmdb.org/t/p/w300\(url)").responseImage { (response) in
+           if let result = response.result.value {
+               completion(result)
+           } else {
+               completion(UIImage())
+           }
+       }
+   }
+   
+   func baixarGeneros(completion: @escaping(generosDetalhados?) -> ()){
+       
+       let url = "https://api.themoviedb.org/3/genre/movie/list?language=pt-BR&api_key=dcfdbbf8648cddebeb0decb1f27aca9a"
+      
+       let request = NSMutableURLRequest(url: NSURL(string: url)! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                                  timeoutInterval: 10.0)
+       request.httpMethod = "GET"
+
+       let dataTask = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                       
+           if let erro = error {
+               print(erro.localizedDescription)
+               completion(nil)
+           }
+                       
+           if let data = data {
+               do{
+                   let json = try JSONDecoder().decode(generosDetalhados.self, from: data)
+                   completion(json)
+               }catch{
+                   print(error.localizedDescription)
+                   completion(nil)
+               }
+           }else{
+               completion(nil)
+           }
+       })
+       
+       dataTask.resume()
+   }
+}
+
+class RequestFavoritos {
+    func tirarFilmeFavorito(index: Int){
+        var favoritos = pegarListaFavoritos()
+        
+        favoritos.remove(at: index)
+        
+        UserDefaults.standard.set(favoritos, forKey: "favoritos")
+        
+    }
+    
+    func salvarFilmeFavorito(id: Int, filme: Filme){
+        
+        var favoritos = pegarListaFavoritos()
+        let indexPossivelFav = verificarFavorito(id: id, filme: filme)
+        
+        if indexPossivelFav == -1 {
+            favoritos.append(filme.filmeDecodable.id ?? 0)
+            UserDefaults.standard.set(favoritos, forKey: "favoritos")
+        }else{
+            tirarFilmeFavorito(index: indexPossivelFav)
+        }
+            
+    }
+    
+    func verificarFavorito(id: Int, filme: Filme) -> Int{
+        
+        let favoritos = pegarListaFavoritos()
+        
+        for (index, favorito) in favoritos.enumerated() {
+            if filme.filmeDecodable.id == favorito {
+                return index
+            }
+        }
+        return -1
+    }
+    
+    func pegarListaFavoritos() -> [Int] {
+        
+        if let idFavoritos = UserDefaults.standard.value(forKey: "favoritos") as? [Int] {
+            return idFavoritos
+        }else{
+           return []
+        }
+        
+    }
+}
