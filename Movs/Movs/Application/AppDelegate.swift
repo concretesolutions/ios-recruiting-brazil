9eb5b9ac231878efb984306211ce5114ca618387
Carolina Lopes <ccal2@cin.ufpe.br>
2019-12-01 17:53:36 -0300

    Add base project

---
+++ Movs/Movs/Application/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  Movs
+//
+//  Created by Carolina Cruz Agra Lopes on 01/12/19.
+//  Copyright © 2019 Carolina Lopes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Variables
+
+    var window: UIWindow?
+
+    // MARK: - App life cycle
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        self.screenSetup()
+
+        return true
+    }
+
+    // MARK: - Screnn setup
+
+    private func screenSetup() {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+    }
+}
---
+++ Movs/Movs/Modules/Controller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Movs
+//
+//  Created by Carolina Cruz Agra Lopes on 01/12/19.
+//  Copyright © 2019 Carolina Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Life cycle
+
+    override func loadView() {
+        self.view = UIView()
+        self.view.backgroundColor = .systemBackground
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
