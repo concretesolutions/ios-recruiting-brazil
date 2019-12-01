11e8947dd1d6e884a64e91808fd62a84e8434f98
Jacqueline Alves <jacque.alves.b@gmail.com>
2019-12-01 14:40:20 -0300

    Tab bar view controller initialization tests

---
+++ movies/moviesTests/TabBarViewControllerSpec.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarViewControllerSpec.swift
+//  moviesTests
+//
+//  Created by Jacqueline Alves on 01/12/19.
+//  Copyright © 2019 jacquelinealves. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import movies
+
+class TabBarViewControllerSpec: QuickSpec {
+    override func spec() {
+        var sut: TabBarViewController!
+        
+        describe("the 'Tab bar' ") {
+            beforeEach {
+                sut = TabBarViewController()
+                _ = sut.view
+            }
+            
+            context("when initialized ") {
+                it("is not nil.") {
+                    expect(sut).toNot(beNil())
+                }
+                
+                it("should have two tab items.") {
+                    expect(sut.viewControllers?.count).to(be(2))
+                }
+            }
+        }
+    }
+}
