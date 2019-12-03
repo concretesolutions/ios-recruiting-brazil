faceda1779b2fbd58a9431b33e06300c6d9d9029
Emerson Victor <emersonvvictor@gmail.com>
2019-12-03 17:43:17 -0300

    Create genres data transfer objects

--- movs/movs/Model/DataTransferObjects/GenresDTO.swift
+++ movs/movs/Model/DataTransferObjects/GenresDTO.swift
@@ -5,5 +5,15 @@
 //  Created by Emerson Victor on 03/12/19.
 //  Copyright © 2019 emer. All rights reserved.
 //
+// swiftlint:disable identifier_name
 
 import Foundation
+
+struct GenresDTO: Codable {
+    let genres: [GenreDTO]
+}
+
+struct GenreDTO: Codable {
+    let id: Int
+    let name: String
+}
