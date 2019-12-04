faa759942c9688248476c48b10fe1968bb96d122
Victor Vieira Veiga <victorvieiraveiga@gmail.com>
2019-12-04 20:38:07 -0300

    Primeiro Commit

---
+++ moviesApp/moviesApp/Suporte/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constantes.swift
+//  moviesApp
+//
+//  Created by Victor Vieira Veiga on 04/12/19.
+//  Copyright © 2019 Victor Vieira Veiga. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    
+    
+    static let baseURL = "https://api.themoviedb.org/3/"
+    static let baseImageUrl =  "https://image.tmdb.org/t/p/w"
+    static let apiKey = "?api_key=da49251803522d8d04033aed26182314"
+    static let moviePath = "discover/movie"
+    
+}
