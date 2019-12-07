282157fd6a20b7754ebd494db705cecf53fc0195
Alexandre Abrahão <alexandre1997pf@hotmail.com>
2019-12-07 20:41:56 -0300

    :heavy_plus_sign: Added Alamofire for networking

--- Podfile
+++ Podfile
@@ -8,6 +8,7 @@ target 'ConcreteChallenge' do
   # Pods for ConcreteChallenge
   pod 'SnapKit', '~> 5.0.0'
   pod 'Kingfisher', '~> 5.11'
+  pod 'Alamofire', '~> 5.0.0-rc.3'
 
   target 'ConcreteChallengeTests' do
     inherit! :complete
--- Pods/Target Support Files/Pods-ConcreteChallenge-ConcreteChallengeTests/Pods-ConcreteChallenge-ConcreteChallengeTests-frameworks.sh
+++ Pods/Target Support Files/Pods-ConcreteChallenge-ConcreteChallengeTests/Pods-ConcreteChallenge-ConcreteChallengeTests-frameworks.sh
@@ -161,10 +161,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
