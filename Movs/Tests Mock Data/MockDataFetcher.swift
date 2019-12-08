bdbdc5f73a4b3c6c06f64c76a241809a8fd0ef51
Carolina Lopes <ccal2@cin.ufpe.br>
2019-12-08 17:12:38 -0300

    Share the mock data fetcher between the test schemes

--- Movs/Tests Mock Data/MockDataFetcher.swift
+++ Movs/Tests Mock Data/MockDataFetcher.swift
@@ -16,6 +16,33 @@ class MockDataFetcher {
     var movies: [Int: [PopularMovieDTO]] = [:]
     var smallImages: [String: String] = [:]
     var bigImages: [String: String] = [:]
+
+    // MARK: - Data mocking
+
+    func mockData() {
+        self.mockGenres()
+        self.mockMovies()
+    }
+
+    func mockGenres() {
+        self.genres = [
+            1: "Action",
+            2: "Romance",
+            3: "Adventure",
+            4: "Comedy",
+            5: "Drama"
+        ]
+    }
+
+    func mockMovies() {
+        self.movies[1] = [
+            PopularMovieDTO(id: 1, title: "Movie_1", overview: "Movie_1 overview", genreIds: [1], releaseDate: "2019", posterPath: "/Movie_1.jpg"),
+            PopularMovieDTO(id: 2, title: "Movie_2", overview: "Movie_2 overview", genreIds: [4], releaseDate: "2010", posterPath: "/Movie_2.jpg")
+        ]
+        self.movies[2] = [
+            PopularMovieDTO(id: 3, title: "Movie_3", overview: "Movie_3 overview", genreIds: [2, 4], releaseDate: "3000", posterPath: nil)
+        ]
+    }
 }
 
 extension MockDataFetcher: MoviesDataFetcherProtocol {
