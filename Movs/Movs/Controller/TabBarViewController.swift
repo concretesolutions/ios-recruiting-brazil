b3f58cce20dd5006b5a331dfdb3dadcf458b3a98
Lucca França <lfgf@cin.ufpe.br>
2019-12-10 17:56:47 -0300

    Change TabBar icons

--- Movs/Movs/Controller/TabBarViewController.swift
+++ Movs/Movs/Controller/TabBarViewController.swift
@@ -13,32 +13,30 @@ class TabBarViewController: UITabBarController {
     private let moviesViewController: MoviesViewController = {
         let moviesViewController = MoviesViewController()
         moviesViewController.title = "Movs"
-
-        moviesViewController.tabBarItem = UITabBarItem(title: "Movs", image: UIImage(named: "movs_tab_3"), tag: 0)
-//        moviesViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 0)
+        moviesViewController.tabBarItem = UITabBarItem(title: "Movies", image: UIImage(systemName: "film"), tag: 0)
         return moviesViewController
     }()
 
     private let favoritesViewController: FavoritesViewController = {
         let favoritesViewController = FavoritesViewController()
         favoritesViewController.title = "Favorites"
-//        favoritesViewController.tabBarItem = UITabBarItem(title: "Movs", image: UIImage(named: ""), tag: 0)
-        favoritesViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        favoritesViewController.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "star"), tag: 0)
         return favoritesViewController
     }()
 
     required init() {
         super.init(nibName: nil, bundle: nil)
         self.viewControllers = [moviesViewController, favoritesViewController].map { navigationController(for: $0) }
-        self.tabBar.unselectedItemTintColor = UIColor.systemOrange.withAlphaComponent(0.3)
-        self.tabBar.isTranslucent = false
+        self.tabBar.isTranslucent = true
         self.tabBar.tintColor = .systemOrange
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    /// Creates and setup a UINavigationController for a UIViewController
+    /// - Parameter controller: UIViewController that will receive the UINavigationController
     private func navigationController(for controller: UIViewController) -> UINavigationController {
         let navigationController = UINavigationController(rootViewController: controller)
         navigationController.navigationBar.prefersLargeTitles = true
