7692695ce0ab3a8a9e50144123b13900515bc684
jonhpol <j.paulo_os@hotmail.com>
2019-12-10 17:32:56 -0300

    [BugFix] change file path

---
+++ Challenge-Concrete/DataSource/MovieCollectionDataSource.swift
@@ -0,0 +1,22 @@
+//
+//  MovieCollectionDataSource.swift
+//  Challenge-Concrete
+//
+//  Created by João Paulo de Oliveira Sabino on 10/12/19.
+//  Copyright © 2019 João Paulo de Oliveira Sabino. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionDataSource: GenericDataSource<Movie>, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //let cell: MovieCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+        //cell.setupData()
+        return UICollectionViewCell()
+    }
+}
