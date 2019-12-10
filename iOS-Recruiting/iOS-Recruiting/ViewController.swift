bcbc5ca39d5148d8ebd7aea626b6c469e8d13a7c
Sev <sivthiago@hotmail.com>
2019-12-10 15:50:27 -0300

    initial project setup

---
+++ iOS-Recruiting/iOS-Recruiting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Recruiting
+//
+//  Created by Thiago Augusto on 10/12/19.
+//  Copyright © 2019 sevontheedge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
