7b43f5fc4ee88ab07483a217025d78eec2242833
kacio reis <khcb@cin.ufpe.br>
2019-12-12 14:32:39 -0300

    add search bar in MoviesController

--- DesafioIos/DesafioIos/MoviesController.swift
+++ DesafioIos/DesafioIos/MoviesController.swift
@@ -9,24 +9,27 @@
 import UIKit
 import SnapKit
 class MoviesController: UIViewController , SendDataApi {
-    func sendMovie(movies: [Movie]) {
-        moviesData.append(contentsOf: movies)
-    }
     var moviesData:[Movie] = [] {
         didSet{
             DispatchQueue.main.async {
+                self.filterMovies = self.moviesData
                 self.collectionView.reloadData()
             }
         }
     }
-   let collectionView: UICollectionView = {
-       let layout = UICollectionViewFlowLayout()
-       layout.scrollDirection = .vertical
-       let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-       cv.backgroundColor = .lightGray
-       return cv
-   }()
-    
+    var filterMovies:[Movie] = []
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .lightGray
+        return cv
+    }()
+    lazy var searchBar: UISearchBar = {
+        let view = UISearchBar(frame: .zero)
+        view.backgroundColor = .black
+        return view
+    }()
     override func loadView() {
         let view = UIView(frame: UIScreen.main.bounds)
         view.backgroundColor = .red
@@ -38,47 +41,67 @@ class MoviesController: UIViewController , SendDataApi {
         setupView()
         setupDataMovie()
     }
-    override func viewWillAppear(_ animated: Bool) {
-    }
     func setupDataMovie(){
         let maneger = ManegerApiRequest()
         maneger.delegate = self
         maneger.sendMovies()
     }
+    func sendMovie(movies: [Movie]) {
+        moviesData.append(contentsOf: movies)
+    }
+
 }
 // MARK: - Protocols of CollectionView
 
 extension MoviesController:UICollectionViewDataSource, UICollectionViewDelegate ,UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.moviesData.count
+        return self.filterMovies.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MovieCellView 
-        cell.movie = moviesData[indexPath.row]
+        cell.movie = self.filterMovies[indexPath.row]
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = self.view.frame.width
         return CGSize(width: (width)/2.5, height: (width)/2)
-     }
+    }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let vc = DetailMovieController()
-        vc.movie = self.moviesData[indexPath.row]
+        vc.movie = self.filterMovies[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 }
+//MARK: - Protocols Search Bar
+extension MoviesController:UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        if searchBar.text == "" {
+            self.filterMovies = self.moviesData
+        } else {
+            filterMovies = self.moviesData.filter({ (movie) -> Bool in
+                return movie.title.lowercased().contains(searchBar.text!.lowercased())
+            })
+        }
+        self.collectionView.reloadData()
+    }
+}
 // MARK: - Protocols CodeView
-
 extension MoviesController:CodeView{
     func buildViewHierarchy() {
         view.addSubview(collectionView)
+        view.addSubview(searchBar)
     }
     
     func setupConstraints() {
         collectionView.snp.makeConstraints { (make) in
-            make.bottom.top.left.right.equalToSuperview()
+            make.bottom.left.right.equalToSuperview()
+            make.top.equalTo(searchBar.snp.bottom)
+        }
+        searchBar.snp.makeConstraints { (make) in
+            make.left.right.equalToSuperview()
+            make.top.equalToSuperview().offset(80)
         }
     }
     
@@ -86,7 +109,7 @@ extension MoviesController:CodeView{
         collectionView.register(MovieCellView.self,forCellWithReuseIdentifier: "cell")
         collectionView.delegate = self
         collectionView.dataSource = self
-
+        searchBar.delegate = self
     }
     
     
