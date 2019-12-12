0ba78438e14d278642881064455a7f6931c87fa9
adriel22 <adriel.arruda@yahoo.com.br>
2019-12-12 00:31:14 -0300

    First Commit

---
+++ ios-recruiting-brazil/Pods/Target Support Files/Pods-ios-recruiting-brazil/Pods-ios-recruiting-brazil-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_recruiting_brazil : NSObject
+@end
+@implementation PodsDummy_Pods_ios_recruiting_brazil
+@end
---
+++ ios-recruiting-brazil/ios-recruiting-brazil/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ios-recruiting-brazil
+//
+//  Created by Adriel Freire on 12/12/19.
+//  Copyright © 2019 Adriel Freire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
