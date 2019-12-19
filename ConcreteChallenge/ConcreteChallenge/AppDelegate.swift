945ef813eddd177ec173f2865806af8f3048828d
Elias Paulino <eliaspaulinoandrade@gmail.com>
2019-12-19 08:40:22 -0300

    view code setup

---
+++ ConcreteChallenge/ConcreteChallenge/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ConcreteChallenge
+//
+//  Created by Elias Paulino on 19/12/19.
+//  Copyright © 2019 Elias Paulino. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ ConcreteChallenge/ConcreteChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConcreteChallenge
+//
+//  Created by Elias Paulino on 19/12/19.
+//  Copyright © 2019 Elias Paulino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
