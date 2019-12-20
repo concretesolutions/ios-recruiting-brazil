11b18cc9f455c46bf35fbca5496f9182e5f34bd1
Carolina Lopes <ccal2@cin.ufpe.br>
2019-12-19 23:31:51 -0300

    Change colors

--- Movs/Movs/Modules/Controller/MovsViewController.swift
+++ Movs/Movs/Modules/Controller/MovsViewController.swift
@@ -19,17 +19,26 @@ class MovsViewController: UITabBarController {
 
     override func viewDidLoad() {
 
+        self.tabBar.tintColor = UIColor(named: "Yellow")
+
         // Popular Movies' screen
 
         let popularNavController = UINavigationController(rootViewController: self.popularMoviesVC)
         popularNavController.tabBarItem = UITabBarItem(title: "Movies", image: UIImage(systemName: "list.bullet"), tag: 0)
+        popularNavController.navigationBar.tintColor = UIColor(named: "Yellow")
         popularNavController.navigationBar.prefersLargeTitles = true
+        popularNavController.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor(named: "Yellow")!]
+        popularNavController.navigationBar.titleTextAttributes = [.foregroundColor: UIColor(named: "Yellow")!]
 
         // Favorite Movies' screen
 
         let favoriteNavController = UINavigationController(rootViewController: self.favoriteMoviesVC)
         favoriteNavController.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "heart"), tag: 0)
+        favoriteNavController.tabBarItem.selectedImage = UIImage(systemName: "heart.fill")
+        favoriteNavController.navigationBar.tintColor = UIColor(named: "Yellow")
         favoriteNavController.navigationBar.prefersLargeTitles = true
+        favoriteNavController.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor(named: "Yellow")!]
+        favoriteNavController.navigationBar.titleTextAttributes = [.foregroundColor: UIColor(named: "Yellow")!]
 
         self.viewControllers = [popularNavController, favoriteNavController]
     }
