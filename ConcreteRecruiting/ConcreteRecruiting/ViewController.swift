ddcb4704bc29dbbc35743ee33d47777752a60959
allymf <alyssonfacanhamoreira@gmail.com>
2019-12-21 20:01:33 -0300

    Add base xcode project

---
+++ ConcreteRecruiting/ConcreteRecruiting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcreteRecruiting
+//
+//  Created by Alysson Moreira on 21/12/19.
+//  Copyright © 2019 Alysson Moreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
