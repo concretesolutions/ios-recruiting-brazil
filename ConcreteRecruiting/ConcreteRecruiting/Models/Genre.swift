121850328035fed4418424f16c74d925ddbf3bdf
allymf <alyssonfacanhamoreira@gmail.com>
2019-12-22 11:56:09 -0300

    Add genre model and use in movie

---
+++ ConcreteRecruiting/ConcreteRecruiting/Models/Genre.swift
@@ -0,0 +1,16 @@
+//
+//  Genre.swift
+//  ConcreteRecruiting
+//
+//  Created by Alysson Moreira on 22/12/19.
+//  Copyright © 2019 Alysson Moreira. All rights reserved.
+//
+
+import Foundation
+
+struct Genre: Decodable {
+    
+    var id: Int
+    var name: String
+    
+}
--- ConcreteRecruiting/ConcreteRecruiting/Models/Movie.swift
+++ ConcreteRecruiting/ConcreteRecruiting/Models/Movie.swift
@@ -13,16 +13,19 @@ struct Movie: Decodable {
     // TODO: Remove the following line
     private let imgBase = "https://image.tmdb.org/t/p/w185/"
     
-    var title: String
-    var description: String
-    var releaseDate: Date
-    var banner: URL?
+    let title: String
+    let description: String
+    let releaseDate: Date
+    let banner: URL?
+    
+    let genres: [Genre]
     
     enum CodingKeys: String, CodingKey {
         case title
         case description = "overview"
         case releaseDate = "release_date"
         case banner = "poster"
+        case genres
     }
     
     init(from decoder: Decoder) throws {
@@ -31,10 +34,11 @@ struct Movie: Decodable {
         title = try values.decode(String.self, forKey: .title)
         description = try values.decode(String.self, forKey: .description)
         releaseDate = try values.decode(Date.self, forKey: .releaseDate)
-        let imgPath = try values.decode(String.self, forKey: .banner)
         
+        let imgPath = try values.decode(String.self, forKey: .banner)
         banner = URL(string: imgBase+imgPath)
         
+        genres = try values.decode([Genre].self, forKey: .genres)
         
 //        let additionalInfo = try values.nestedContainer(keyedBy: AdditionalInfoKeys.self, forKey: .additionalInfo)
 //        elevation = try additionalInfo.decode(Double.self, forKey: .elevation)
