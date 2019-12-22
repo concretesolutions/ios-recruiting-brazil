d940f46c12f50dcf99190339e3185fafc0b8f8ee
allymf <alyssonfacanhamoreira@gmail.com>
2019-12-21 23:01:05 -0300

    Add temporary convenience extension to add multiple subviews

---
+++ ConcreteRecruiting/ConcreteRecruiting/Common/Extensions/UIKit/UIView+AddSubviews.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+AddSubviews.swift
+//  ConcreteRecruiting
+//
+//  Created by Alysson Moreira on 21/12/19.
+//  Copyright © 2019 Alysson Moreira. All rights reserved.
+//
+
+import Foundation
