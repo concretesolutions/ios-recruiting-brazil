28c54b3f4970cf6d0156b6b3888b5f3103cc6804
allymf <alyssonfacanhamoreira@gmail.com>
2019-12-22 18:23:58 -0300

    Add enums

---
+++ ConcreteRecruiting/NetworkLayer/Enums/HTTPTask.swift
@@ -0,0 +1,21 @@
+//
+//  HTTPTask.swift
+//  NetworkLayer
+//
+//  Created by Alysson Moreira on 22/12/19.
+//  Copyright © 2019 Alysson Moreira. All rights reserved.
+//
+
+import Foundation
+
+public typealias Parameters = [String: String]
+
+public enum HTTPTask {
+    
+    case requestPlain
+    case requestUrlParameters(Parameters)
+    case requestBodyParameters(Parameters)
+    case requestUrlBodyParameters(Parameters)
+    
+    
+}
---
+++ ConcreteRecruiting/NetworkLayer/Enums/NetworkError.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkError.swift
+//  NetworkLayer
+//
+//  Created by Alysson Moreira on 22/12/19.
+//  Copyright © 2019 Alysson Moreira. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case unknown
+    case noJson
+    case apiError
+    
+}
