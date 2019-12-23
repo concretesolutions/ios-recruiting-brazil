78fedcb41d5f7fe9e0907fec8e1a54ad966c09e3
allymf <alyssonfacanhamoreira@gmail.com>
2019-12-23 16:13:00 -0300

    Implement the function to get the list of popular movies

--- ConcreteRecruiting/ConcreteRecruiting/Services/NetworkManager.swift
+++ ConcreteRecruiting/ConcreteRecruiting/Services/NetworkManager.swift
@@ -7,11 +7,21 @@
 //
 
 import Foundation
+import NetworkLayer
 
 class NetworkManager {
     
     static let baseUrl = "https://api.themoviedb.org/3/movie/"
     static let baseImageUrl = "https://image.tmdb.org/t/p/"
     static let apiKey = "d07322200307dc499064d61f72cbee14"
+    
+    private let router = Router<MovieEndPoint>()
+    
+    func getPopularMovies(page: Int, completion: @escaping (Result<[Movie], Error>
+        ) -> Void) {
+        
+        router.request(.popularMovies, type: [Movie].self, completion: completion)
+        
+    }
     
 }
