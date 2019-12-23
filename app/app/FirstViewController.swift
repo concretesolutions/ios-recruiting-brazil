9b7901cb82ac099f0b27d8e784534632290d703d
Renan Freitas <rfl3@cin.ufpe.br>
2019-12-23 17:16:39 -0300

    Create base project

---
+++ app/app/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  app
+//
+//  Created by rfl3 on 23/12/19.
+//  Copyright © 2019 Renan Freitas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ app/appTests/appTests.swift
@@ -0,0 +1,34 @@
+//
+//  appTests.swift
+//  appTests
+//
+//  Created by rfl3 on 23/12/19.
+//  Copyright © 2019 Renan Freitas. All rights reserved.
+//
+
+import XCTest
+@testable import app
+
+class appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
