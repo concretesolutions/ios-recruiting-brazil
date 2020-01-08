a1f9c3d2c33bc3f1aa0834e074865aea63335a97
Renato Lopes <renattoolopes@gmail.com>
2020-01-07 20:53:21 -0400

    Created project and gitignore

---
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Renato Lopes on 07/01/20.
+//  Copyright © 2020 renato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
