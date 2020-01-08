2dd6efa0868fa2f846fc41792f5e31d6dbef9c4f
Renato Lopes <renattoolopes@gmail.com>
2020-01-07 22:07:04 -0400

    Created movies and favorites controllers

---
+++ TheMovieDB/TheMovieDB/View/Favorites/FavoritesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesViewController.swift
+//  TheMovieDB
+//
+//  Created by Renato Lopes on 07/01/20.
+//  Copyright © 2020 renato. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FavoritesViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ TheMovieDB/TheMovieDB/View/Movies/MoviesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesViewController.swift
+//  TheMovieDB
+//
+//  Created by Renato Lopes on 07/01/20.
+//  Copyright © 2020 renato. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MoviesViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
