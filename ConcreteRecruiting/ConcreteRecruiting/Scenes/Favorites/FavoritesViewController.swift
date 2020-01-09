4edf6044b2c8ec6b9bf9758e355dc98e156dab0b
allymf <alyssonfacanhamoreira@gmail.com>
2020-01-09 02:13:05 -0300

    Add filter barbutton

--- ConcreteRecruiting/ConcreteRecruiting/Scenes/Favorites/FavoritesViewController.swift
+++ ConcreteRecruiting/ConcreteRecruiting/Scenes/Favorites/FavoritesViewController.swift
@@ -15,18 +15,12 @@ class FavoritesViewController: UIViewController {
 
         self.view = FavoritesView()
         
-        // Do any additional setup after loading the view.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "Filter"), style: .plain, target: self, action: #selector(didTapFilter))
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func didTapFilter() {
+        
     }
-    */
 
 }
