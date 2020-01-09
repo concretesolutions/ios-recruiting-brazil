999cc171250ded46818436fe7ba350d269fea33a
allymf <alyssonfacanhamoreira@gmail.com>
2020-01-09 02:35:15 -0300

    Create a datasource for the favorite with mock data

---
+++ ConcreteRecruiting/ConcreteRecruiting/Scenes/Favorites/FavoritesDataSource.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesDataSource.swift
+//  ConcreteRecruiting
+//
+//  Created by Alysson Moreira on 09/01/20.
+//  Copyright © 2020 Alysson Moreira. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesDataSource: NSObject, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? FavoriteTableViewCell else {
+            fatalError("Unable to dequeue a cell with the Cell Identifier")
+        }
+        
+        return cell
+        
+    }
+    
+    
+    
+    
+}
