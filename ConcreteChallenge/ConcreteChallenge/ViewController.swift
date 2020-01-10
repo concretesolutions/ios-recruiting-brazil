3d1e18059c3a8bba2098a31cc895cc9c7ade8f58
Kaique D'amato <kaiquedamato@icloud.com>
2020-01-10 15:06:07 -0300

    Initial Commit

---
+++ ConcreteChallenge/ConcreteChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcreteChallenge
+//
+//  Created by Kaique Damato on 10/1/20.
+//  Copyright © 2020 KiQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
