8a6edd96fd38e9bb250aad9c0116ef266e04ee94
Renato Lopes <renattoolopes@gmail.com>
2020-01-11 18:19:36 -0400

    Created manager to request informations API and method GET

--- TheMovieDB/TheMovieDB/Service/ServiceAPIManager.swift
+++ TheMovieDB/TheMovieDB/Service/ServiceAPIManager.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class ServiceAPIManager {
+    struct PathsAPI {
+        public static let key = "6192cea18be6726e8bf48424e7b43b27"
+        public static let https = "https://"
+        public static let rootAPI = "api.themoviedb.org"
+        public static let versionAPI = "/3"
+        public static let api_key = "api_key"
+        public static let language = "language"
+        public static let page = "page"
+        
+        struct MovieAPI {
+            public static let movie = "/movie"
+            public static let popular = "/popular"
+        }
+        
+        public static func getKeyPath() -> String {
+            return "\(PathsAPI.api_key)\(PathsAPI.key)"
+        }
+    }
+    
+    public static let session = URLSession(configuration: .default)
+    
+    public static func get(url: URL, completition: @escaping (_ data: Data?, _ error: Error?) -> Void ) {
+        session.dataTask(with: url) { (data, response, error) in
+            completition(data, error)
+        }.resume()
+    }
+}
