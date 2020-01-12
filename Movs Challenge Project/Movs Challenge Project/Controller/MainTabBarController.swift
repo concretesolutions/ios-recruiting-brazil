f1a150232adf800bf95151eee3c067767068176e
Jezreel Barbosa <jezreel.barbosa.42@gmail.com>
2020-01-12 14:33:44 -0400

    Added main tab bar controller into the project

---
+++ Movs Challenge Project/Movs Challenge Project/Controller/MainTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabBarController.swift
+//  Movs Challenge Project
+//
+//  Created by Jezreel de Oliveira Barbosa on 12/01/20.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    // Static Properties
+    // Static Methods
+    // Public Types
+    // Public Properties
+    // Public Methods
+    // Initialisation/Lifecycle Methods
+    
+    convenience init() {
+        self.init(nibName: nil, bundle: nil)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        initController()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initController()
+    }
+    
+    // Override Methods
+    // Private Types
+    // Private Properties
+    // Private Methods
+    
+    private func initController() {
+        
+    }
+}
