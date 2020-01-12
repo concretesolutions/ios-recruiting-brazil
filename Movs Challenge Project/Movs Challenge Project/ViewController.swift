0018c620ec4039a0ff46d6e275b16774d81342a7
Jezreel Barbosa <jezreel.barbosa.42@gmail.com>
2020-01-11 20:29:43 -0400

    Created the project to work in the project

---
+++ Movs Challenge Project/Movs Challenge Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movs Challenge Project
+//
+//  Created by Jezreel de Oliveira Barbosa on 11/01/20.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movs Challenge Project/Movs Challenge ProjectTests/Movs_Challenge_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Movs_Challenge_ProjectTests.swift
+//  Movs Challenge ProjectTests
+//
+//  Created by Jezreel de Oliveira Barbosa on 11/01/20.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import XCTest
+@testable import Movs_Challenge_Project
+
+class Movs_Challenge_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
