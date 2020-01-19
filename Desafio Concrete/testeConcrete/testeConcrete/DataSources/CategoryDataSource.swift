a313555ac2fc729b5c940264c90bc6dff85e31da
Unknown <neto3333@gmail.com>
2020-01-19 01:37:56 -0400

    Criados novos Datasources.

--- Desafio Concrete/testeConcrete/testeConcrete/DataSources/CategoryDataSource.swift
+++ Desafio Concrete/testeConcrete/testeConcrete/DataSources/CategoryDataSource.swift
@@ -8,6 +8,39 @@
 
 import UIKit
 
-class CategoryDataSource: UITableViewDataSource {
-
+class CategoryDataSource: NSObject{
+    
+    var list:[Category] = []
+    
+    override init(){
+        super.init()
+        loadList()
+    }
 }
+
+extension CategoryDataSource{
+    func loadList(){
+        CategoryService.getCategory(){
+            response in
+            guard let data = response.success else{
+                return
+            }
+            self.list = data
+        }
+    }
+}
+
+extension CategoryDataSource:UITableViewDataSource{
+    
+    
+      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          list.count
+      }
+      
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel!.text = list[indexPath.row].name
+        return cell
+    }
+}
+
--- Desafio Concrete/testeConcrete/testeConcrete/DataSources/OptionFilterDataSource.swift
+++ Desafio Concrete/testeConcrete/testeConcrete/DataSources/OptionFilterDataSource.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import UIKit
+
+class OptionFilterDataSource:NSObject,UITableViewDataSource{
+    let options = ["Genero","Data"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "memes")
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel!.text = options[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    
+}
