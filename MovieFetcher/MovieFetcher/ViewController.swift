3b40e9004ed97772fb273f2bf446d2c5166e6ec7
Unknown <pedro_808_@hotmail.com>
2020-01-22 17:18:28 -0300

    Project created

---
+++ MovieFetcher/MovieFetcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFetcher
+//
+//  Created by Pedro Azevedo on 22/01/20.
+//  Copyright © 2020 Pedro Azevedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
