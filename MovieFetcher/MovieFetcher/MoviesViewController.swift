65c66b2e9f258599e7966aa1c5436e94a04948f9
Unknown <pedro_808_@hotmail.com>
2020-01-22 18:16:27 -0300

    Created tab bar

---
+++ MovieFetcher/MovieFetcher/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesViewController.swift
+//  MovieFetcher
+//
+//  Created by Pedro Azevedo on 22/01/20.
+//  Copyright © 2020 Pedro Azevedo. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MovieFetcher/MovieFetcher/TabBarController.swift
+++ MovieFetcher/MovieFetcher/TabBarController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
