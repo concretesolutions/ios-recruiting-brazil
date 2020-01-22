4b28b50f647b809eec09c45c405da7c482d95ff0
Unknown <pedro_808_@hotmail.com>
2020-01-22 17:27:02 -0300

    Removed main.storyboard to avoid using storyboardsa

--- MovieFetcher/MovieFetcher/SceneDelegate.swift
+++ MovieFetcher/MovieFetcher/SceneDelegate.swift
@@ -13,12 +13,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+           // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+           // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+           // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+           guard let _ = (scene as? UIWindowScene) else { return }
+           let viewController = ViewController()
+           if let windowScene = scene as? UIWindowScene {
+               self.window = UIWindow(windowScene: windowScene); self.window?.rootViewController = viewController; self.window?.makeKeyAndVisible()
+               
+           }
+       }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
