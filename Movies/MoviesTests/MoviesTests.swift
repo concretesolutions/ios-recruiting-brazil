d062063e6021aa4d3265280d0817969c9bb91fec
Mariaelena <mariaelenasilveira@info.ufrn.br>
2020-01-27 23:03:18 -0300

    rename project

--- Movies/MoviesTests/MoviesTests.swift
+++ Movies/MoviesTests/MoviesTests.swift
@@ -7,9 +7,9 @@
 //
 
 import XCTest
-@testable import Muv_Concrete
+@testable import Movies
 
-class Muv_ConcreteTests: XCTestCase {
+class MoviesTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
