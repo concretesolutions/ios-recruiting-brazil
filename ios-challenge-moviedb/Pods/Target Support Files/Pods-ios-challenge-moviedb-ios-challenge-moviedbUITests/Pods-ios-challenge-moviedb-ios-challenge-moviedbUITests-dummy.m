7241642b6a0aed9f40a0fd0882458bc779f42e6b
Giovanni Severo <giosvro@gmail.com>
2020-02-20 18:57:56 -0300

    :building_construction: Architectural changes

---
+++ ios-challenge-moviedb/Pods/Target Support Files/Pods-ios-challenge-moviedb-ios-challenge-moviedbUITests/Pods-ios-challenge-moviedb-ios-challenge-moviedbUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_challenge_moviedb_ios_challenge_moviedbUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ios_challenge_moviedb_ios_challenge_moviedbUITests
+@end
