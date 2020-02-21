55fb25c5de3fe1e435ffa8a5f3f79bbae49d7c34
Giovanni Severo <giosvro@gmail.com>
2020-02-21 18:42:29 -0300

    :construction: Created Movies and Genre for API

---
+++ ios-challenge-moviedb/ios-challenge-moviedb/Model/Genre.swift
@@ -0,0 +1,28 @@
+//
+//  Genre.swift
+//  ios-challenge-moviedb
+//
+//  Created by Giovanni Severo Barros on 21/02/20.
+//  Copyright © 2020 Giovanni Severo Barros. All rights reserved.
+//
+
+import Foundation
+
+class Genre {
+    
+    var id: Int
+    var name: String
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
+
+extension Genre {
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+    }
+}
---
+++ ios-challenge-moviedb/ios-challenge-moviedb/Model/Movie.swift
@@ -0,0 +1,44 @@
+//
+//  Movie.swift
+//  ios-challenge-moviedb
+//
+//  Created by Giovanni Severo Barros on 21/02/20.
+//  Copyright © 2020 Giovanni Severo Barros. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    
+    var id: Int
+    var title: String
+    var overview: String
+    var posterPath: String
+    var backdropPath: String
+    var releaseDate: String
+    var genres: [Genre]
+    
+    init(id: Int, title: String, overview: String, posterPath: String,
+         backdropPath: String, releaseDate: String, genres: [Genre]) {
+        self.id = id
+        self.title = title
+        self.overview = overview
+        self.posterPath = posterPath
+        self.backdropPath = backdropPath
+        self.releaseDate = releaseDate
+        self.genres = genres
+    }
+}
+
+extension Movie {
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case overview
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+        case releaseDate = "release_date"
+        case genres
+    }
+}
