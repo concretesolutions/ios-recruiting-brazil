247519ab5847e0a0be738c099b3ed431e293b272
Giovanni Severo <giosvro@gmail.com>
2020-03-02 18:11:39 -0300

    :white_check_mark: Movie Client Tests

--- ios-challenge-moviedb/ios-challenge-moviedbTests/MovieClientTests.swift
+++ ios-challenge-moviedb/ios-challenge-moviedbTests/MovieClientTests.swift
@@ -6,4 +6,57 @@
 //  Copyright © 2020 Giovanni Severo Barros. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import Movies
+
+class MovieClientTests: XCTestCase {
+    
+    var movies: [Movie]?
+    var genres: [Genre]?
+    var error: Error?
+    
+    override func setUp() {
+        super.setUp()
+        movies = nil
+        genres = nil
+        error = nil
+    }
+    
+    override func tearDown() {
+        movies = nil
+        genres = nil
+        error = nil
+        super.tearDown()
+    }
+    
+    func testGetAllPopularMovies() {
+        
+        let promise = expectation(description: "Get All Popular Movies Done")
+        
+        MovieClient.getPopularMovies(page: 1) { (movies, error) in
+            self.movies = movies?.movies
+            self.error = error
+            promise.fulfill()
+        }
+        
+        wait(for: [promise], timeout: 10)
+        
+        XCTAssertNil(error, "Get All Popular Movies Failed")
+        XCTAssertNotNil(movies, "No movies received")
+    }
+    
+    func testGetAllGenres() {
+        let promise = expectation(description: "Get All Genres")
+        
+        MovieClient.getAllGenres { (genres, error) in
+            self.genres = genres
+            self.error = error
+            promise.fulfill()
+        }
+        
+        wait(for: [promise], timeout: 10)
+        
+        XCTAssertNil(error, "Get All Popular Movies Failed")
+        XCTAssertNotNil(genres, "No genres received")
+    }
+}
