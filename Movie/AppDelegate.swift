cb1ba07a1e985e2564ba4fb3612fc3f4649372cd
Gabriel Reynoso <gabriel.n.reynoso@concrete.com.br>
2020-03-03 11:19:14 -0300

    Create XCode project with needed resources

---
+++ Movie/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession,
+                     options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Movie Scene", sessionRole: connectingSceneSession.role)
+    }
+}
