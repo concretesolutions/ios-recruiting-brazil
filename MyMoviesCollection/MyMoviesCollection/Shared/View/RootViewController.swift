6bb756909a2ab3856625a0b37fb1dc7a0bb6c3a4
Filipe Merli <filipemerli@air-de-filipe.home>
2020-03-12 00:02:17 -0300

    App Icon, Splash Screen and StoryBoard deleted

---
+++ MyMoviesCollection/MyMoviesCollection/Shared/View/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewController.swift
+//  MyMoviesCollection
+//
+//  Created by Filipe Merli on 11/03/20.
+//  Copyright © 2020 Filipe Merli. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+
+}
