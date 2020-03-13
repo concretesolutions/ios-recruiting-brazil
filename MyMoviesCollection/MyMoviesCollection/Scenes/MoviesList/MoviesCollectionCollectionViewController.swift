8ed1e4562839cfc4c8b93d130850d0d3d059e90c
filipe.merli <filipe.merli@agilecontent.com>
2020-03-12 22:18:50 -0300

    TabBar itens configured

--- MyMoviesCollection/MyMoviesCollection/Scenes/MoviesList/MoviesCollectionCollectionViewController.swift
+++ MyMoviesCollection/MyMoviesCollection/Scenes/MoviesList/MoviesCollectionCollectionViewController.swift
@@ -15,6 +15,7 @@ class MoviesCollectionCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.collectionView.backgroundColor = .red
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
--- MyMoviesCollection/MyMoviesCollection/Shared/View/MainTabBarViewController.swift
+++ MyMoviesCollection/MyMoviesCollection/Shared/View/MainTabBarViewController.swift
@@ -20,11 +20,13 @@ class MainTabBarViewController: UITabBarController {
     // MARK: Class functions
     
     private func configView() {
+        self.tabBar.barTintColor = UIColor(red: 247.0 / 255.0, green: 206.0 / 255.0, blue: 91.0 / 255.0, alpha: 1.0)
+        self.tabBar.tintColor = UIColor(red: 45.0 / 255.0, green: 48.0 / 255.0, blue: 71.0 / 255.0, alpha: 1.0)
         let moviesCollectionViewController = MoviesCollectionCollectionViewController(collectionViewLayout: MoviesListFlowLayout())
-        let tabBar1 = UITabBarItem(tabBarSystemItem: .downloads, tag: 0)
+        let tabBar1 = UITabBarItem(title: "Movies", image: #imageLiteral(resourceName: "list_icon"), tag: 0)
         moviesCollectionViewController.tabBarItem = tabBar1
         let favoritesTableViewController = FavoritesTableViewController()
-        let tabBar2 = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        let tabBar2 = UITabBarItem(title: "Favorites", image: #imageLiteral(resourceName: "favorite_empty_icon"), tag: 1)
         favoritesTableViewController.tabBarItem = tabBar2
         self.viewControllers = [moviesCollectionViewController, favoritesTableViewController]
     }
