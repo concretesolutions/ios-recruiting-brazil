d04972d1191537dcada7ef5f6f10bc60350988f6
gus-svr <giogusbr@gmail.com>
2020-04-18 17:21:06 -0300

    :sparkles: Setup Xcode Project

---
+++ Movies-Browser/Movies-Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies-Browser
+//
+//  Created by Gustavo Severo on 17/04/20.
+//  Copyright © 2020 Severo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movies-Browser/Movies-BrowserTests/Movies_BrowserTests.swift
@@ -0,0 +1,34 @@
+//
+//  Movies_BrowserTests.swift
+//  Movies-BrowserTests
+//
+//  Created by Gustavo Severo on 17/04/20.
+//  Copyright © 2020 Severo. All rights reserved.
+//
+
+import XCTest
+@testable import Movies_Browser
+
+class Movies_BrowserTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
