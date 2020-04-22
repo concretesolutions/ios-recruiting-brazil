0257749a02a69160a15bdf832bd415507045cffa
gus-svr <giogusbr@gmail.com>
2020-04-21 22:20:06 -0300

    :lipstick: Movie Detail Module

--- Movies-Browser/Movies-Browser/Scenes/MovieDetail/MovieDetailViewController.swift
+++ Movies-Browser/Movies-Browser/Scenes/MovieDetail/MovieDetailViewController.swift
@@ -7,15 +7,80 @@
 //
 
 import UIKit
+import SDWebImage
 
 class MovieDetailViewController: UIViewController {
     // MARK: - Variables
     weak var coordinator: MovieDetailCoordinator?
+    weak var viewModel: MovieDetailViewModel?
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var genresLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var favoriteButton: UIButton!
+    
+    //MARK: - IBActions
+    @IBAction func favoriteAction(_ sender: Any) {
+        viewModel?.favoriteButtonWasTapped()
+    }
+}
+
+// MARK: - ViewModel -
+extension MovieDetailViewController {
+    func setupViewModelListener(){
+        viewModel?.callback = { [weak self] state in
+            self?.posterImageURLDidChange(state.posterImageURL)
+            self?.titleTextDidChange(state.titleText)
+            self?.releaseDateTextDidChange(state.releaseDateText)
+            self?.genresListTextDidChange(state.genreListText)
+            self?.descriptionTextDidChange(state.descriptionText)
+            self?.isFavoriteDidChange(state.isFavorite)
+        }
+    }
 }
 
 // MARK: - Events -
 extension MovieDetailViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupViewModelListener()
+    }
+}
+
+// MARK: - State UI Updates -
+extension MovieDetailViewController {
+    func posterImageURLDidChange(_ url: String){
+        imageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
+        imageView.sd_setImage(with: URL(string: url), placeholderImage: UIImage(named: "placeholder"))
+    }
+    
+    func isFavoriteDidChange(_ state: Bool){
+        if state {
+            favoriteButton.setTitle("Remove from the favorite list", for: .normal)
+            favoriteButton.setImage(UIImage(named: "star-filled")?.withRenderingMode(.alwaysTemplate), for: .normal)
+            
+        } else {
+            favoriteButton.setTitle("Add to the favorite list", for: .normal)
+            favoriteButton.setImage(UIImage(named: "star")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        }
+    }
+    
+    func titleTextDidChange(_ text: String){
+        self.titleLabel.text = text
+    }
+    
+    func releaseDateTextDidChange(_ text: String){
+        self.releaseDateLabel.text = text
+    }
+    
+    func genresListTextDidChange(_ text: String){
+        self.genresLabel.text = text
+    }
+    
+    func descriptionTextDidChange(_ text: String){
+        self.descriptionLabel.text = text
     }
 }
--- Movies-Browser/Movies-Browser/Scenes/MovieDetail/MovieDetailViewModel.swift
+++ Movies-Browser/Movies-Browser/Scenes/MovieDetail/MovieDetailViewModel.swift
@@ -6,8 +6,67 @@
 //  Copyright © 2020 Severo. All rights reserved.
 //
 
-import Foundation
+struct MovieDetailState {
+    // MARK: Data
+    var posterImageURL: String
+    var titleText: String
+    var releaseDateText: String
+    var genreListText: String
+    var descriptionText: String
+    var isFavorite: Bool
+}
 
 final class MovieDetailViewModel {
+    let database = Database()
+    private var movie: Movie {
+        didSet {
+            movieValueDidChange()
+        }
+    }
     
+    var state: MovieDetailState = MovieDetailState(posterImageURL: "", titleText: "", releaseDateText: "", genreListText: "", descriptionText: "", isFavorite: false) {
+        didSet {
+            callback?(state)
+        }
+    }
+    
+    var callback: ((MovieDetailState) -> ())? {
+        didSet {
+            callback?(state)
+        }
+    }
+    
+    public init(movie: Movie, callback: ((MovieDetailState) -> ())?) {
+        self.callback = callback
+        self.movie = movie
+        self.movieValueDidChange()
+    }
+}
+
+// MARK: - Inputs -
+extension MovieDetailViewModel {
+    func favoriteButtonWasTapped(){
+        state.isFavorite.toggle()
+        updateFavoriteDatabaseState()
+    }
+    
+    func movieValueDidChange(){
+        state.posterImageURL = AppConstants.API.imageBaseUrl + movie.posterPath
+        state.titleText = movie.title
+        state.descriptionText = movie.overview
+        state.releaseDateText = movie.releaseDate.year
+        state.isFavorite = database.isFavorited(id: movie.id)
+        state.genreListText = database.getGenresListString(ids: movie.genreIds)
+    }
+}
+
+// MARK: - Database -
+extension MovieDetailViewModel {
+    func updateFavoriteDatabaseState(){
+        if state.isFavorite {
+            database.addNewFavorite(movie: movie)
+        } else {
+            database.deleteFavorite(id: movie.id)
+        }
+    }
 }
