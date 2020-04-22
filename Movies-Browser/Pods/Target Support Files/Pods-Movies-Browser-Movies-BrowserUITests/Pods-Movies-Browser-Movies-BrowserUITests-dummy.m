f635bc29aef542d3148f96bac066f0508f00e257
gus-svr <giogusbr@gmail.com>
2020-04-21 22:12:14 -0300

    :arrow_up: Adding CocoaPods

---
+++ Movies-Browser/Pods/Target Support Files/Pods-Movies-Browser-Movies-BrowserUITests/Pods-Movies-Browser-Movies-BrowserUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Movies_Browser_Movies_BrowserUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Movies_Browser_Movies_BrowserUITests
+@end
---
+++ Movies-Browser/Pods/Target Support Files/Pods-Movies-BrowserTests/Pods-Movies-BrowserTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Movies_BrowserTests : NSObject
+@end
+@implementation PodsDummy_Pods_Movies_BrowserTests
+@end
