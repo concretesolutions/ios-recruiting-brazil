8a977e332f39f602429e8de09ec325e81fccddba
João Lucas <joaolucasfp2001@gmail.com>
2020-10-08 20:44:14 -0300

    Created Movies DTO

---
+++ Movs/Modules/MoviesList/DTO/MoviesDTO.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesDTO.swift
+//  Movs
+//
+//  Created by Joao Lucas on 08/10/20.
+//
+
+import Foundation
+
+struct MoviesDTO: Decodable {
+    let results: [ResultMoviesDTO]
+}
+
+struct ResultMoviesDTO: Decodable {
+    let id: Int
+    let title: String
+    let poster_path: String
+}
---
+++ Movs/Modules/MoviesList/Service/MoviesListProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesListProtocol.swift
+//  Movs
+//
+//  Created by Joao Lucas on 08/10/20.
+//
+
+import Foundation
