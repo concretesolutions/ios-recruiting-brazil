d2eac36982dbcf24741c587e9e3a88c8088cf400
João Lucas <joaolucasfp2001@gmail.com>
2020-10-12 11:47:26 -0300

    Added title in favorites controller

--- Movs/Modules/Favorites/Controller/FavoritesController.swift
+++ Movs/Modules/Favorites/Controller/FavoritesController.swift
@@ -6,22 +6,52 @@
 //
 
 import UIKit
+import RealmSwift
 
 class FavoritesController: UITableViewController {
-
+    
+    private let realm = try! Realm()
+    private var itemsFavorites = [FavoriteEntity]()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "favoriteList")
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        itemsFavorites = realm.objects(FavoriteEntity.self).map({ $0 })
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseListFavorites")
+        tableView.tableFooterView = UIView.init(frame: .zero)
+        
+        refresh()
+    }
+    
+    private func refresh() {
+        itemsFavorites = realm.objects(FavoriteEntity.self).map({ $0 })
+        tableView.reloadData()
+    }
+}
+
+extension FavoritesController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsFavorites.count
+    }
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "favoriteList", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseListFavorites", for: indexPath)
+        
+        let item = itemsFavorites[indexPath.row]
+        
+        cell.textLabel?.text = item.title
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
     }
 }
