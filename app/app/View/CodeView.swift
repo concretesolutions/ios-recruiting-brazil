fe9004a9453613c7b6b8f6c60337919fe9c608f0
Renan Freitas <rfl3@cin.ufpe.br>
2020-10-15 03:12:13 -0300

    Code View Protocol

---
+++ app/app/View/CodeView.swift
@@ -0,0 +1,24 @@
+//
+//  CodeView.swift
+//  app
+//
+//  Created by rfl3 on 15/10/20.
+//  Copyright © 2020 renacio. All rights reserved.
+//
+
+import Foundation
+
+protocol CodeView: class {
+    func buildViewHierarchy()
+    func setupConstraints()
+    func setupAdditionalConfiguration()
+    func setupLayout()
+}
+
+extension CodeView {
+    func setupLayout() {
+        buildViewHierarchy()
+        setupConstraints()
+        setupAdditionalConfiguration()
+    }
+}
