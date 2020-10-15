c4f3f5951ea1820af8f1887e6477c1a0ee119338
Renan Freitas <rfl3@cin.ufpe.br>
2020-10-15 07:02:02 -0300

    Add Nimble and Quick pods

---
+++ app/appUITests/SearchBarViewSpec.swift
@@ -0,0 +1,10 @@
+//
+//  SearchBarViewSpec.swift
+//  appUITests
+//
+//  Created by rfl3 on 15/10/20.
+//  Copyright © 2020 renacio. All rights reserved.
+//
+
+import Nimble_Snapshots
+import Quick
