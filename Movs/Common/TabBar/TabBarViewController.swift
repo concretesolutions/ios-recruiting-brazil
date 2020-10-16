17b673b8b779be72c4cdfd9f1de74d0a67e403d2
João Lucas <joaolucasfp2001@gmail.com>
2020-10-16 14:03:45 -0300

    Organization code TabBar

--- Movs/Common/TabBar/TabBarViewController.swift
+++ Movs/Common/TabBar/TabBarViewController.swift
@@ -10,42 +10,42 @@ import UIKit
 class TabBarViewController: UITabBarController {
 
     private let moviesController: MoviesListController = {
-            let layout = UICollectionViewFlowLayout()
-            let moviesController = MoviesListController(collectionViewLayout: layout)
-            moviesController.title = "Movs"
-            moviesController.tabBarItem = UITabBarItem(title: "Movs", image: UIImage(named: "list_icon"), tag: 0)
-            return moviesController
-        }()
+        let layout = UICollectionViewFlowLayout()
+        let moviesController = MoviesListController(collectionViewLayout: layout)
+        moviesController.title = "Movs"
+        moviesController.tabBarItem = UITabBarItem(title: "Movs", image: UIImage(named: "list_icon"), tag: 0)
+        return moviesController
+    }()
 
-        private let favoritesController: FavoritesController = {
-            let favoritesController = FavoritesController()
-            favoritesController.title = "Favorites"
-            favoritesController.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(named: "favorite_empty_icon"), tag: 1)
-            return favoritesController
-        }()
+    private let favoritesController: FavoritesController = {
+        let favoritesController = FavoritesController()
+        favoritesController.title = "Favorites"
+        favoritesController.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(named: "favorite_empty_icon"), tag: 1)
+        return favoritesController
+    }()
         
-        required init() {
-            super.init(nibName: nil, bundle: nil)
-        }
+    required init() {
+        super.init(nibName: nil, bundle: nil)
+    }
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-            viewControllers = [moviesController, favoritesController].map { navigationController(for: $0) }
-            tabBar.isTranslucent = true
-            tabBar.tintColor = .systemBlue
-            tabBar.barTintColor = .white
-        }
+        viewControllers = [moviesController, favoritesController].map { navigationController(for: $0) }
+        tabBar.isTranslucent = true
+        tabBar.tintColor = .systemBlue
+        tabBar.barTintColor = .white
+    }
         
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
-        private func navigationController(for controller: UIViewController) -> UINavigationController {
-            let navigationController = UINavigationController(rootViewController: controller)
-            navigationController.navigationBar.prefersLargeTitles = true
-            navigationController.navigationItem.largeTitleDisplayMode = .always
-            navigationController.navigationBar.barTintColor = .white
-            return navigationController
-        }
+    private func navigationController(for controller: UIViewController) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: controller)
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.navigationItem.largeTitleDisplayMode = .always
+        navigationController.navigationBar.barTintColor = .white
+        return navigationController
+    }
 }
