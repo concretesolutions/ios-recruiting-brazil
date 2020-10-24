a9ab2402abd1058dfa7bac47b4c3eff955c7ecb6
Adrian Almeida <drialme@zup002107-nb.itau.corp.ihf>
2020-10-24 14:39:17 -0300

    correct load viewcontroller

--- ConcreteChallenge/ConcreteChallenge/Sources/SceneDelegate.swift
+++ ConcreteChallenge/ConcreteChallenge/Sources/SceneDelegate.swift
@@ -23,14 +23,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
 
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController()
+        window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
 
         appCoordinator?.start()
-
-        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
