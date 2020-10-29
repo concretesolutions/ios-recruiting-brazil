a28de97d953e44c6e52a7828d67370d2cb439a1c
Adrian Almeida <drialme@zup002107-nb.itau.corp.ihf>
2020-10-29 01:57:50 -0300

    create worker realm worker and protocol

---
+++ ConcreteChallenge/ConcreteChallenge/Sources/Scenes/Movies/MoviesRealmWorker.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesRealmWorker.swift
+//  ConcreteChallenge
+//
+//  Created by Adrian Almeida on 29/10/20.
+//  Copyright © 2020 Adrian Almeida. All rights reserved.
+//
+
+import Foundation
+
+final class MoviesRealmWorker: MoviesRealmWorkerProtocol {
+
+    // MARK: - Conforms MoviesRealmWorkerProtocol
+
+    func saveMovie(movie: Movie) { }
+}
---
+++ ConcreteChallenge/ConcreteChallenge/Sources/Scenes/Movies/Protocols/MoviesRealmWorkerProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  MoviesRealmWorkerProtocol.swift
+//  ConcreteChallenge
+//
+//  Created by Adrian Almeida on 29/10/20.
+//  Copyright © 2020 Adrian Almeida. All rights reserved.
+//
+
+protocol MoviesRealmWorkerProtocol: AnyObject {
+    func saveMovie(movie: Movie)
+}
