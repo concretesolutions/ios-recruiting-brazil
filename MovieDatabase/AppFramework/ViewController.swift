4e9f0cd3d92f8eadb598c8dff8a3c00ef8822a45
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-17 17:09:05 -0300

    refact: move ViewController to AppFramework

---
+++ MovieDatabase/AppFramework/ViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+public class ViewController: UIViewController {
+  override public func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
--- MovieDatabase/MovieDatabase/AppDelegate.swift
+++ MovieDatabase/MovieDatabase/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AppFramework
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
