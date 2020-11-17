a52437e4730370b2e258e5d1250985d574934ecf
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-17 17:08:08 -0300

    build: create AppFramework

---
+++ MovieDatabase/AppFramework/AppFramework.h
@@ -0,0 +1,18 @@
+//
+//  AppFramework.h
+//  AppFramework
+//
+//  Created by Aleph Retamal on 17/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppFramework.
+FOUNDATION_EXPORT double AppFrameworkVersionNumber;
+
+//! Project version string for AppFramework.
+FOUNDATION_EXPORT const unsigned char AppFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppFramework/PublicHeader.h>
+
+
