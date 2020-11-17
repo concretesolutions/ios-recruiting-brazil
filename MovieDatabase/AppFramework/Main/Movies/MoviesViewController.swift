cc7d63c7ba97a67a70a75f27d20113a3fe0d3a99
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-17 20:40:16 -0300

    ci: setup node environment with husky
    - Added pre-commit hook configuration with husky

--- MovieDatabase/AppFramework/Main/Movies/MoviesViewController.swift
+++ MovieDatabase/AppFramework/Main/Movies/MoviesViewController.swift
@@ -1,13 +1,64 @@
+import Combine
 import UIKit
 
 public final class MoviesViewController: UICollectionViewController {
-  public init() {
-    let layout = UICollectionViewFlowLayout()
+  typealias DataSource = UICollectionViewDiffableDataSource<Int, Int>
+  typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Int>
+
+  private var cancellables = Set<AnyCancellable>()
+
+  private lazy var dataSource: DataSource = {
+    DataSource(collectionView: collectionView) { collectionView, indexPath, _ -> UICollectionViewCell? in
+      let cell: UICollectionViewCell = collectionView.dequeue(for: indexPath)
+      cell.backgroundColor = .yellow
+      return cell
+    }
+  }()
+
+  private static func makeCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+    let itemSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(0.46),
+      heightDimension: .fractionalHeight(1.0)
+    )
+
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+    let groupSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(1),
+      heightDimension: .absolute(200)
+    )
+
+    let group = NSCollectionLayoutGroup.horizontal(
+      layoutSize: groupSize,
+      subitems: [item]
+    )
+    group.interItemSpacing = NSCollectionLayoutSpacing.flexible(16)
+    group.edgeSpacing = NSCollectionLayoutEdgeSpacing(leading: nil, top: nil, trailing: nil, bottom: .fixed(24))
+
+    let section = NSCollectionLayoutSection(group: group)
+    section.contentInsets = NSDirectionalEdgeInsets(top: 24, leading: 24, bottom: 24, trailing: 24)
+
+    let layout = UICollectionViewCompositionalLayout(section: section)
+
+    return layout
+  }
+
+  init() {
+    let layout = Self.makeCollectionViewLayout()
     super.init(collectionViewLayout: layout)
   }
 
   @available(*, unavailable)
   required init?(coder _: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+
+  override public func viewDidLoad() {
+    super.viewDidLoad()
+    collectionView.register(UICollectionViewCell.self)
+    var snapshot = Snapshot()
+    snapshot.appendSections([0])
+    snapshot.appendItems(Array(0 ... 10))
+    dataSource.apply(snapshot)
   }
 }
--- MovieDatabase/README.md
+++ MovieDatabase/README.md
@@ -2,6 +2,16 @@
 
 ### Environment
 
-1. Install Mint by following the instructions [here](https://github.com/yonaskolb/Mint#installing)
+**Node (optional)**
+
+We use husky to setup git hooks. To use husky you'll need to have a node environment setup. 
+
+1. Install and setup [nvm](https://github.com/nvm-sh/nvm)
+2. Install the node version specified on [.nvmrc](.nvmrc)
+3. run `npm install`
+
+**Mint and Swift Packages (optional)**
+
+1. Install and setup [Mint](https://github.com/yonaskolb/Mint#installing)
 2. run `mint bootstrap`
 
