9a22632d2d570c179672ba1b6fd079e67d4a49b9
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-18 17:20:32 -0300

    build: create TheMovieDatabaseApi framework target

---
+++ MovieDatabase/TheMovieDatabaseApi/TheMovieDatabaseApi.h
@@ -0,0 +1,18 @@
+//
+//  TheMovieDatabaseApi.h
+//  TheMovieDatabaseApi
+//
+//  Created by Aleph Retamal on 18/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TheMovieDatabaseApi.
+FOUNDATION_EXPORT double TheMovieDatabaseApiVersionNumber;
+
+//! Project version string for TheMovieDatabaseApi.
+FOUNDATION_EXPORT const unsigned char TheMovieDatabaseApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheMovieDatabaseApi/PublicHeader.h>
+
+
