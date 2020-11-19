0935c4198c5de1d219807e6a8c53f75b0de7cc4d
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-19 16:53:27 -0300

    feat: make response codable and constructur public

--- MovieDatabase/TheMovieDatabaseApi/Sources/Endpoints/DiscoverMovie.swift
+++ MovieDatabase/TheMovieDatabaseApi/Sources/Endpoints/DiscoverMovie.swift
@@ -8,19 +8,45 @@ public struct DiscoverMovieRequestParams {
   }
 }
 
-public struct DiscoverMovieResponse: Decodable, Equatable {
-  public struct Movie: Decodable, Equatable {
+public struct DiscoverMovieResponse: Codable, Equatable {
+  public struct Movie: Codable, Equatable {
     public let id: Int
     public let title: String
     public let overview: String
     public let posterPath: String
     public let releaseDate: String
+
+    public init(
+      id: Int,
+      title: String,
+      overview: String,
+      posterPath: String,
+      releaseDate: String
+    ) {
+      self.id = id
+      self.title = title
+      self.overview = overview
+      self.posterPath = posterPath
+      self.releaseDate = releaseDate
+    }
   }
 
   public let page: Int
   public let totalResults: Int
   public let totalPages: Int
   public let results: [Movie]
+
+  public init(
+    page: Int,
+    totalResults: Int,
+    totalPages: Int,
+    results: [Movie]
+  ) {
+    self.page = page
+    self.totalResults = totalResults
+    self.totalPages = totalPages
+    self.results = results
+  }
 }
 
 extension Request {
