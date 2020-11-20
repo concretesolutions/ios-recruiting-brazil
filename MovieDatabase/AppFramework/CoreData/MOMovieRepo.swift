81c1c221ca3146e79545d9abb6eed6e5cc469082
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-20 15:11:15 -0300

    feat: add debug logging on database actions

--- MovieDatabase/AppFramework/CoreData/MOMovieRepo.swift
+++ MovieDatabase/AppFramework/CoreData/MOMovieRepo.swift
@@ -23,6 +23,11 @@ public struct MOMovieRepo {
         fetchRequest.predicate = NSPredicate(format: "id = %i", id)
         do {
           let results = try moc.fetch(fetchRequest)
+          if results.first != nil {
+            os_log("Fetched MOMovie with id %i", log: generalLog, type: .debug, id)
+          } else {
+            os_log("Tryed to fetch MOMovie with id %i, but it doesn't exist", log: generalLog, type: .debug, id)
+          }
           return results.first
         } catch {
           os_log("Failed to get MOMovie with id %i, reason: %s", log: generalLog, type: .error, id, error.localizedDescription)
@@ -34,6 +39,7 @@ public struct MOMovieRepo {
         applyChanges(&movie)
         do {
           try moc.save()
+          os_log("Created MOMovie with id %i", log: generalLog, type: .debug, movie.id)
           return movie
         } catch {
           os_log("Failed to create MOMovie, reason: %s", log: generalLog, type: .error, error.localizedDescription)
@@ -44,6 +50,7 @@ public struct MOMovieRepo {
         do {
           moc.delete(movie)
           try moc.save()
+          os_log("Deleted MOMovie with id %i", log: generalLog, type: .debug, movie.id)
           return true
         } catch {
           os_log("Failed to delete MOMovie with id %i, reason: %s", log: generalLog, type: .error, movie.id, error.localizedDescription)
