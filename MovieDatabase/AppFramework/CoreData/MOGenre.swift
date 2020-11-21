f46e3807d5b895b689e585bd6af27a622f5ee617
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-21 12:22:59 -0300

    feat: add MOGenre model to CoreData

---
+++ MovieDatabase/AppFramework/CoreData/MOGenre.swift
@@ -0,0 +1,6 @@
+import CoreData
+
+public final class MOGenre: NSManagedObject {
+  @NSManaged public var id: Int16
+  @NSManaged public var name: String
+}
--- MovieDatabase/AppFramework/CoreData/MOMovie.swift
+++ MovieDatabase/AppFramework/CoreData/MOMovie.swift
@@ -3,5 +3,7 @@ import CoreData
 public final class MOMovie: NSManagedObject {
   @NSManaged public var id: Int64
   @NSManaged public var title: String
+  @NSManaged public var year: String
+  @NSManaged public var genres: Set<MOGenre>
   @NSManaged public var overview: String
 }
