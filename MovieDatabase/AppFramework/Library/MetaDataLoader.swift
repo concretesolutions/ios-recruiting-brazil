504a2711e4d900b934c4068de41edf6435e2dc4b
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-21 17:18:50 -0300

    feat: load genres from coredata if request failed

--- MovieDatabase/AppFramework/Library/MetaDataLoader.swift
+++ MovieDatabase/AppFramework/Library/MetaDataLoader.swift
@@ -1,5 +1,6 @@
 import Combine
 import CoreData
+import os.log
 import TheMovieDatabaseApi
 
 private func loadConfig() -> AnyPublisher<Result<RemoteConfiguration, ErrorResponse>, Never> {
@@ -32,6 +33,9 @@ public class MetaDataLoader {
   public let metaData: AnyPublisher<MetaData, Never>
 
   public init() {
+    let genreRepo = MOGenreRepo.default(moc: Env.database.moc)
+    let currentSavedGenres = genreRepo.getAll()
+
     let config = _reloadConfig
       .map { _ in loadConfig() }
       .switchToLatest()
@@ -39,6 +43,43 @@ public class MetaDataLoader {
     let genres = _reloadGenres
       .map { _ in loadGenres() }
       .switchToLatest()
+      .handleEvents(receiveOutput: { result in
+        if case let .success(genres) = result, genres.count > 0 {
+          let currentSavedIds = Set(currentSavedGenres.map(\.id))
+          let fromServerIds = Set(genres.map(\.id))
+          let changes = currentSavedIds
+            .symmetricDifference(fromServerIds)
+            .intersection(currentSavedIds)
+
+          if changes.count == 0 { return }
+
+          genres
+            .filter { changes.contains($0.id) }
+            .forEach { genre in
+              let moGenre = MOGenre(context: Env.database.moc)
+              moGenre.id = genre.id
+              moGenre.name = genre.name
+            }
+
+          if genres.count == 0 { return }
+
+          do {
+            try Env.database.moc.save()
+            os_log("Saved MOGenres from server", log: generalLog, type: .debug)
+          } catch {
+            os_log("Failed to save MOGenres, reason: %s", log: generalLog, type: .error, error.localizedDescription)
+          }
+        }
+      })
+      .map { result -> Result<[Genre], ErrorResponse> in
+        if case .failure = result {
+          let genres = genreRepo.getAll()
+            .map { genre in Genre(id: genre.id, name: genre.name) }
+          return .success(genres)
+        }
+
+        return result
+      }
 
     metaData = Publishers.CombineLatest(config, genres)
       .map { configResult, genresResult -> MetaData in
