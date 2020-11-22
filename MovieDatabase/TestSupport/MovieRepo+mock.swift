c473636a1b9bedf6493f646c526bb9403a76ef40
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-22 14:42:11 -0300

    fix: TestSupport MOMovieRepo -> MovieRepo

--- MovieDatabase/TestSupport/MovieRepo+mock.swift
+++ MovieDatabase/TestSupport/MovieRepo+mock.swift
@@ -2,7 +2,7 @@ import AppFramework
 
 public extension MovieRepo {
   static func mock(
-    get: @escaping (Int) -> MOMovie? = { _ in nil },
+    get: @escaping (Int64) -> MOMovie? = { _ in nil },
     getAll: @escaping () -> [MOMovie] = { [] },
     create: @escaping ((inout MOMovie) -> Void) -> MOMovie? = { _ in nil },
     delete: @escaping (MOMovie) -> Bool = { _ in false }
