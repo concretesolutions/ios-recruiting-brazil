15216497d8e99a132224929d67706e28c0d90872
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-22 16:04:12 -0300

    feat: simplify save/remove logic on MovieViewModel

--- MovieDatabase/AppFramework/App/Main/Movies/MovieViewModel.swift
+++ MovieDatabase/AppFramework/App/Main/Movies/MovieViewModel.swift
@@ -44,27 +44,20 @@ public struct MovieViewModel: Identifiable, Hashable {
     repo: MovieRepo
   ) -> MovieViewModel {
     MovieViewModel(movie: movie) { input in
-      var _moMovie = repo.get(movie.id)
-      var currentLike = _moMovie != nil
+      let initialLiked = repo.get(movie.id) != nil
 
       // TODO: This logic can be improved. We probably don't need to
       // delete and save the context right after a click
       let like = input.like
-        .scan(currentLike) { liked, _ in !liked }
-        .prepend(currentLike)
+        .scan(initialLiked) { liked, _ in !liked }
         .handleEvents(receiveOutput: { liked in
           if liked {
-            if _moMovie == nil {
-              _moMovie = repo.create(movie)
-            }
+            _ = repo.create(movie)
           } else {
-            if let movieUnwrapped = _moMovie {
-              _ = repo.delete(movieUnwrapped)
-              _moMovie = nil
-            }
+            _ = repo.delete(movie)
           }
-          currentLike = liked
         })
+        .prepend(initialLiked)
 
       return Output(
         like: like.eraseToAnyPublisher()
