e3c4acf09a6f6f5d4340abcb9bfe2ed9fca28696
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-22 17:39:26 -0300

    feat: create Design framework, move Splash image

---
+++ MovieDatabase/Design/Design.h
@@ -0,0 +1,18 @@
+//
+//  Design.h
+//  Design
+//
+//  Created by Aleph Retamal on 22/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Design.
+FOUNDATION_EXPORT double DesignVersionNumber;
+
+//! Project version string for Design.
+FOUNDATION_EXPORT const unsigned char DesignVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Design/PublicHeader.h>
+
+
