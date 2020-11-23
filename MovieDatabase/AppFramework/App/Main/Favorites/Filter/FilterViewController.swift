c68776f8b950efec1ea816b6ad4ffc7f26c0e1d7
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-23 20:09:54 -0300

    feat: add apply button to FiltersVC

--- MovieDatabase/AppFramework/App/Main/Favorites/Filter/FilterViewController.swift
+++ MovieDatabase/AppFramework/App/Main/Favorites/Filter/FilterViewController.swift
@@ -3,11 +3,13 @@ import UIKit
 
 enum FilterSection: Hashable {
   case main
+  case apply
 }
 
 enum FilterItem: Hashable {
   case date(String?)
   case genres(Int?)
+  case apply
 }
 
 final class FilterCell: UITableViewCell {
@@ -22,6 +24,33 @@ final class FilterCell: UITableViewCell {
   }
 }
 
+final class ButtonCell: UITableViewCell {
+  private let button = UIButton(type: .system)
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    selectionStyle = .none
+    contentView.addSubview(button)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      button.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+      button.topAnchor.constraint(equalTo: contentView.topAnchor),
+      button.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+      button.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+      button.heightAnchor.constraint(equalToConstant: 44),
+    ])
+  }
+
+  @available(*, unavailable)
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  func setup(with title: String) {
+    button.setTitle(title, for: .normal)
+  }
+}
+
 public final class FilterViewController: UITableViewController {
   typealias DataSource = UITableViewDiffableDataSource<FilterSection, FilterItem>
   typealias Snapshot = NSDiffableDataSourceSnapshot<FilterSection, FilterItem>
@@ -33,18 +62,24 @@ public final class FilterViewController: UITableViewController {
 
   private lazy var dataSource: DataSource = {
     DataSource(tableView: tableView, cellProvider: { tableView, indexPath, item in
-      let cell: FilterCell = tableView.dequeue(for: indexPath)
       switch item {
       case let .date(date):
+        let cell: FilterCell = tableView.dequeue(for: indexPath)
         cell.textLabel?.text = L10n.Screen.Favorites.Filter.date
         cell.detailTextLabel?.text = date
+        return cell
       case let .genres(quantity):
+        let cell: FilterCell = tableView.dequeue(for: indexPath)
         cell.textLabel?.text = L10n.Screen.Favorites.Filter.genres
         cell.detailTextLabel?.text = quantity == nil
           ? nil
           : "\(quantity!) genres chosen"
+        return cell
+      case .apply:
+        let cell: ButtonCell = tableView.dequeue(for: indexPath)
+        cell.setup(with: "Apply")
+        return cell
       }
-      return cell
     })
   }()
 
@@ -61,13 +96,16 @@ public final class FilterViewController: UITableViewController {
     super.viewDidLoad()
     title = L10n.Screen.Favorites.Filter.title
     tableView.register(FilterCell.self)
+    tableView.register(ButtonCell.self)
 
     var snapshot = Snapshot()
-    snapshot.appendSections([.main])
+    snapshot.appendSections([.main, .apply])
     snapshot.appendItems([
       .date(nil),
       .genres(nil),
     ], toSection: .main)
+
+    snapshot.appendItems([.apply], toSection: .apply)
     dataSource.apply(snapshot)
   }
 
@@ -79,9 +117,11 @@ public final class FilterViewController: UITableViewController {
   }
 
   override public func tableView(_: UITableView, didSelectRowAt indexPath: IndexPath) {
-    if indexPath.row == 0 {
-    } else if indexPath.row == 1 {
-      _presentGenres.send(())
+    guard let item = dataSource.itemIdentifier(for: indexPath) else { return }
+    switch item {
+    case .date: break
+    case .genres: _presentGenres.send(())
+    case .apply: break
     }
   }
 
