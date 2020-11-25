0dd45de7b4dc55134907fbbd02d4877bae5462b6
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-11-25 13:22:15 -0300

    tests: improve ergonomics of PaginationSinkTests

--- MovieDatabase/AppFrameworkTests/Library/PaginationSinkTests.swift
+++ MovieDatabase/AppFrameworkTests/Library/PaginationSinkTests.swift
@@ -1,142 +1,128 @@
 import AppFramework
 import Combine
+import Foundation
 import XCTest
 
-struct TestError: Error, Equatable {}
-
 class PaginationSinkTests: XCTestCase {
-  var cancellables: Set<AnyCancellable>!
-
-  override func setUpWithError() throws {
-    cancellables = .init()
-  }
-
-  override func tearDownWithError() throws {
-    cancellables = nil
-  }
-
-  func testPaginationSink_RefreshAfterNextPage_ShouldResetValuesAndCursor() throws {
-    let _refresh = PassthroughSubject<Void, Never>()
-    let _nextPage = PassthroughSubject<Void, Never>()
-
-    let paginationSink = PaginationSink<Int, Never>.make(
-      refreshTrigger: _refresh.share().eraseToAnyPublisher(),
-      nextPageTrigger: _nextPage.share().eraseToAnyPublisher(),
+  func testPaginationSink_NextPageTwice() throws {
+    assertPaginationSink(
       valuesFromEnvelope: { [$0] },
       cursorFromEnvelope: { $0 },
-      requestFromCursor: { cursor in Future<Result<Int, Never>, Never>({ promise in promise(.success(.success(cursor))) }).eraseToAnyPublisher() }
+      requestFromCursor: { cursor in
+        Future<Result<Int, Never>, Never>({ promise in
+          promise(.success(.success(cursor)))
+
+        }).eraseToAnyPublisher()
+      },
+      actions: { input in
+        input.refresh.send(())
+        input.nextPage.send(())
+        input.nextPage.send(())
+      },
+      assertions: [
+        .isRefreshing(false),
+        .isLoadingNextPage(false),
+        .isRefreshing(true),
+        .isRefreshing(false),
+        .values([1]),
+        .isLoadingNextPage(true),
+        .isLoadingNextPage(false),
+        .values([1, 2]),
+        .isLoadingNextPage(true),
+        .isLoadingNextPage(false),
+        .values([1, 2, 3]),
+      ]
     )
-
-    let valuesExpectation = expectation(description: "value")
-
-    paginationSink.values
-      .collect(5)
-      .sink(receiveValue: { values in
-        XCTAssertEqual(values, [
-          [1],
-          [1, 2],
-          [1],
-          [1, 2],
-          [1, 2, 3],
-        ])
-        valuesExpectation.fulfill()
-      })
-      .store(in: &cancellables)
-
-    _refresh.send(())
-    _nextPage.send(())
-    _refresh.send(())
-    _nextPage.send(())
-    _nextPage.send(())
-
-    waitForExpectations(timeout: 1.0, handler: nil)
   }
 
-  func testPaginationSink_RequestFailure_ShouldSendAnError() throws {
-    let _refresh = PassthroughSubject<Void, Never>()
-    let _nextPage = PassthroughSubject<Void, Never>()
-
-    let paginationSink = PaginationSink<Int, TestError>.make(
-      refreshTrigger: _refresh.share().eraseToAnyPublisher(),
-      nextPageTrigger: _nextPage.share().eraseToAnyPublisher(),
+  func testPaginationSink_NextPageErroring() throws {
+    assertPaginationSink(
       valuesFromEnvelope: { envelope in [envelope] },
       cursorFromEnvelope: { cursor in cursor },
       requestFromCursor: { cursor -> AnyPublisher<Result<Int, TestError>, Never> in
-        let result: Result<Int, TestError> = cursor > 4
+        let result: Result<Int, TestError> = cursor > 1
           ? .failure(.init())
           : .success(cursor)
 
         return Future<Result<Int, TestError>, Never>({ promise in promise(.success(result)) }).eraseToAnyPublisher()
-      }
+      },
+      actions: { input in
+        input.refresh.send(())
+        input.nextPage.send(())
+        input.nextPage.send(())
+      },
+      assertions: [
+        .isRefreshing(false),
+        .isLoadingNextPage(false),
+        .isRefreshing(true),
+        .isRefreshing(false),
+        .values([1]),
+        .isLoadingNextPage(true),
+        .isLoadingNextPage(false),
+        .error(TestError()),
+        .isLoadingNextPage(true),
+        .isLoadingNextPage(false),
+        .error(TestError()),
+      ]
     )
+  }
+}
 
-    let valuesExpectation = expectation(description: "value")
-    let errorExpectation = expectation(description: "error")
+private struct TestError: Error, Equatable {}
 
-    paginationSink.values
-      .collect(4)
-      .sink(receiveValue: { values in
-        XCTAssertEqual(values, [
-          [1],
-          [1, 2],
-          [1, 2, 3],
-          [1, 2, 3, 4],
-        ])
-        valuesExpectation.fulfill()
-      })
-      .store(in: &cancellables)
+private struct PaginationSinkInput {
+  let refresh = PassthroughSubject<Void, Never>()
+  let nextPage = PassthroughSubject<Void, Never>()
+}
 
-    paginationSink.error
-      .collect(2)
-      .sink(receiveValue: { errors in
-        XCTAssertEqual([
-          TestError(),
-          TestError(),
-        ], errors)
-        errorExpectation.fulfill()
-      })
-      .store(in: &cancellables)
-
-    _refresh.send(())
-    _nextPage.send(())
-    _nextPage.send(())
-    _nextPage.send(())
-    _nextPage.send(())
-    _nextPage.send(())
-
-    waitForExpectations(timeout: 1.0, handler: nil)
+private enum PaginationSinkEvent<Value: Equatable, Err: Swift.Error>: Equatable {
+  static func == (lhs: PaginationSinkEvent<Value, Err>, rhs: PaginationSinkEvent<Value, Err>) -> Bool {
+    switch (lhs, rhs) {
+    case let (.isRefreshing(l), .isRefreshing(r)): return l == r
+    case let (.isLoadingNextPage(l), .isLoadingNextPage(r)): return l == r
+    case let (.values(l), .values(r)): return l == r
+    case let (.error(l), .error(r)): return l.localizedDescription == r.localizedDescription
+    default: return false
+    }
   }
 
-  func testPaginationSink_RequestFailure_ShouldNotSendValues() throws {
-    let _refresh = PassthroughSubject<Void, Never>()
-    let _nextPage = PassthroughSubject<Void, Never>()
+  case isRefreshing(Bool)
+  case isLoadingNextPage(Bool)
+  case values([Value])
+  case error(Err)
+}
 
-    let paginationSink = PaginationSink<Int, TestError>.make(
-      refreshTrigger: _refresh.share().eraseToAnyPublisher(),
-      nextPageTrigger: _nextPage.share().eraseToAnyPublisher(),
-      valuesFromEnvelope: { envelope in [envelope] },
-      cursorFromEnvelope: { cursor in cursor },
-      requestFromCursor: { _ -> AnyPublisher<Result<Int, TestError>, Never> in
-        Future<Result<Int, TestError>, Never>({ promise in promise(.success(.failure(TestError()))) }).eraseToAnyPublisher()
-      }
-    )
+private func assertPaginationSink<Value: Equatable, Err: Error, Envelope>(
+  file: StaticString = #file,
+  line: UInt = #line,
+  valuesFromEnvelope: @escaping (Envelope) -> [Value],
+  cursorFromEnvelope: @escaping (Envelope) -> Int,
+  requestFromCursor: @escaping (Int) -> AnyPublisher<Result<Envelope, Err>, Never>,
+  actions: @escaping (PaginationSinkInput) -> Void,
+  assertions: [PaginationSinkEvent<Value, Err>]
+) {
+  let input = PaginationSinkInput()
+  let paginationSink = PaginationSink<Int, Err>.make(
+    refreshTrigger: input.refresh.eraseToAnyPublisher(),
+    nextPageTrigger: input.nextPage.eraseToAnyPublisher(),
+    valuesFromEnvelope: valuesFromEnvelope,
+    cursorFromEnvelope: cursorFromEnvelope,
+    requestFromCursor: requestFromCursor
+  )
 
-    let errorExpectation = expectation(description: "error")
+  let publisher = Publishers.Merge4(
+    paginationSink.isRefreshing.map(PaginationSinkEvent<Value, Err>.isRefreshing),
+    paginationSink.isLoadingNextPage.map(PaginationSinkEvent<Value, Err>.isLoadingNextPage),
+    paginationSink.values.map(PaginationSinkEvent<Value, Err>.values),
+    paginationSink.error.map(PaginationSinkEvent<Value, Err>.error)
+  )
+  .eraseToAnyPublisher()
 
-    paginationSink.values
-      .sink(receiveValue: { _ in
-        XCTFail()
-      })
-      .store(in: &cancellables)
-
-    paginationSink.error
-      .sink(receiveValue: { _ in
-        errorExpectation.fulfill()
-      })
-      .store(in: &cancellables)
-
-    _refresh.send(())
-
-    waitForExpectations(timeout: 1.0, handler: nil)
-  }
+  assertPublisherEvents(
+    file: file,
+    line: line,
+    publisher: publisher,
+    trigger: { actions(input) },
+    assertions: assertions
+  )
 }
