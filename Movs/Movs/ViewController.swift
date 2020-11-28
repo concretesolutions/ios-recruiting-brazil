53cf257b0dfd017b328c834f9387833c0c10bf00
Gabriel Coutinho <gcoutinhorj@gmail.com>
2020-11-28 00:59:21 -0300

    assets, splashscreen e inicio do projeto

---
+++ Movs/Movs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movs
+//
+//  Created by Gabriel Coutinho on 28/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
